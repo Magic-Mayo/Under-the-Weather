4ce579f70819df80166bca22b6110cf0e7c9bf9f
Magic-Mayo <mike.mayoii@gmail.com>
2019-09-30 18:14:12 -0700

    added fb login button for testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
-
+function App() {
+  return (
+    <div className="App">
+        <div className="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+    </div>
+  );
+}
 
 export default App;
