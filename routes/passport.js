60508dfbbd1abdb151ddb9476f3c78ed3dcba118
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-03 17:31:26 -0700

    set up auth for facebook, google and local and started taking info from facebook and storing it to create a new user

--- routes/passport.js
+++ routes/passport.js
@@ -1,14 +1,21 @@
 require('dotenv').config();
 const moment = require('moment');
-const db = require('../models/User.js');
+const db = require('../models');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const FBStrategy = require('passport-facebook').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-const Local = passport.use(new LocalStrategy(
+passport.serializeUser(function(user, done) {
+    done(null, user);
+   });
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use(new LocalStrategy(
     function(username, password, done) {
-        User.findOne({ username: username }, function (err, user) {
+        db.User.findOne({ username: username }, (err, user)=>{
         if (err) { return done(err); }
         if (!user || !user.validPassword(password)) {
             return done(null, false, { message: 'Please verify you are using the correct user name and password combination' });
@@ -18,49 +25,75 @@ const Local = passport.use(new LocalStrategy(
     }
 ));
 
-const facebook = passport.use(new FBStrategy({
+passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID || FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET || FACEBOOK_APP_SECRET,
-    callbackURL: 'localhost:3000/auth/facebook/callback' || "https://under-the-weather.herokuapp.com/auth/facebook/callback"
+    callbackURL: '/auth/facebook/callback' || "https://under-the-weather.herokuapp.com/auth/facebook/callback"
     },
     function(accessToken, refreshToken, profile, done) {
-    db.User.findOrCreate(profile, function(err, user) {
+    db.User.findOne({userName: profile.id}, (err, user)=>{
         if (err) { return done(err); }
-        done(null, user);
+        if(!user){
+            db.User.create({
+                userName: profile.id,
+                name: profile.displayName
+            }).then(user=>{done(user)})
+        } else {
+            done(user);
+        }
     });
     }
 ));
 
-const google = passport.use(new GoogleStrategy({
+passport.use(new GoogleStrategy({
     clientID:  process.env.GOOGLE_CLIENT_ID || GOOGLE_CLIENT_ID,
     clientSecret:  process.env.GOOGLE_CLIENT_SECRET || GOOGLE_CLIENT_SECRET,
     callbackURL: 'localhost:3000/auth/google/callback' || "https://under-the-weather.herokuapp.com/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
-        db.User.findOrCreate({ googleId: profile.id }, function (err, user) {
-            return done(err, user);
+        db.User.findOne({ userName: profile.id }, (err, user)=>{
+            if(!user){
+                db.User.create({
+                    userName: profile.id
+                }).then(user=>{return done(err, user)})
+            } else {
+                return done(err, user);
+            }
         });
     }
 ));
 
 module.exports = (app) => {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     app.post('/login', passport.authenticate('local', {
-            successRedirect: `/home`,
+            successRedirect: '/home',
             failureRedirect: '/',
             failureFlash: 'Please verify you are using the correct user name and password combination'
         }),
-        (req,res)=>{
+        (req,res,next)=>{
             res.redirect(`/users/${req.user.username}`);
         }
     );
 
-    app.get('/auth/facebook', passport.authenticate('facebook'));
-    app.get('auth/facebook/callback', passport.authenticate('facebook', {
-        successRedirect: '/home',
-        failureRedirect: '/'
-    }));
-    app.get('/auth/google', passport.authenticate('google'));
-    app.get('auth/google/callback', passport.authenticate('google', { scope: 'https://www.google.com/m8/feeds' }));
+    app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['public_profile','email']}));
+    app.get('/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: '/'},
+        (req,res,next)=>{
+            res.redirect('/home')
+        }
+    ));
+
+    app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+    app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/', session: false }),
+        function(req, res) {
+
+            res.redirect('/');
+        }
+    );
     
     app.get('/users/:user', (req,res)=>{
         db.User.findOne({where: {userName: req.params.user}}).then(user=>{
