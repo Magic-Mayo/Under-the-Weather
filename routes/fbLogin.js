381e7d03fb609f0661806fd371e634d4e587e809
MagicMayo <mike.mayoii@gmail.com>
2019-10-08 21:53:04 -0700

    finished route for facebook to get data

--- routes/fbLogin.js
+++ routes/fbLogin.js
@@ -10,27 +10,23 @@ module.exports = (app) => {
         console.log('auth')
     });
 
-    app.get('auth/facebook/callback', (req,res)=>{
-        console.log('cb')
-    });
-
     app.get('/login/', (req,res)=>{
         const token = req.query.code
         const urlState = req.query.state
-        // console.log(urlState)
-        // console.log(token)
-        if (urlState===state){
+        if (urlState === state){
             axios.get(`https://graph.facebook.com/v4.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${token}`)
-                .then(user=>{
-                    // console.log(user.data)                    
-                    axios.get(`graph.facebook.com/debug_token?input_token=${user.data.access_token}&access_token=${process.env.FACEBOOK_APP_TOKEN}`)
-                        .then(user=>console.log(user)).catch(err=>console.log(err))
-                }).catch(err=>console.log(err))
-            // res.json('hello')
-            // .redirect();
-        } else {
-            // console.log(req)
+                .then(code=>{
+                    console.log(code.data)
+                    axios.get(`https://graph.facebook.com/debug_token?input_token=${code.data.access_token}&access_token=${process.env.FACEBOOK_APP_TOKEN}`)
+                        .then(user=>{
+                            axios.get(`https://graph.facebook.com/${user.data.data.user_id}?fields=id,email,name&access_token=${code.data.access_token}`)
+                                .then(data=>{
+                                    console.log(data)
+                                })
+                        })
+                        .catch(err=>console.log(err))
+                })
+                .catch(err=>console.log(err))
         }
-        // console.log(req)
     });    
 }
