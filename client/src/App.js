548650f210d39b6d38bf70d7bb01f871e26fb7b8
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-08 18:25:09 -0700

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,43 @@ import React, {Component} from 'react';
 import './App.css';
 import axios from 'axios';
 
+// if (window.location.href.split('?')[1] && window.location.href.split('?')[1].substring(0,4) === 'code'){
+//     axios.get(`/login/${window.location.href.split('?')[1]}`).then(async user=>{
+//         const newUser = await user;
+//         console.log(newUser)
+//     })
+
+// }
 
 class App extends Component {
 
-    facebook = () => {
-        axios.get('/auth/facebook')
+    state = {
+        name: '',
+        password: ' '
     }
+
+    facebook = (e) => {
+        e.preventDefault();
+        console.log('hi');
+        axios.get('/auth/facebook').then(user=>console.log(user))
+    }
+
+    handleChange = (event) => {
+        const {name, value} = event.target
+        this.setState({[name]: value})
+        console.log(this.state)
+    }
+
 
     render() {
         return (
             <div className="App">
-                <p onClick={this.facebook}>Facebook login</p>
+                {/* <form id='form'>
+                    <input name='name' placeholder='name' onChange={this.handleChange} value={this.state.name}></input>
+                    <input name='password' placeholder='password'onChange={this.handleChange} value={this.state.password}></input>
+                    <button type='submit' onClick={this.facebook}>Facebook login</button>
+                </form> */}
+                <a href='http://localhost:3001/auth/facebook'>Facebook</a>
             </div>
         );
     }
