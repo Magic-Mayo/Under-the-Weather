ee7c5c06b554ab0f7dece5a50310b4e077808b0f
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-10 14:47:42 -0700

    undo changes. wrong branch

--- server.js
+++ server.js
@@ -4,32 +4,25 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
 const session = require("express-session");
 const bodyParser = require("body-parser");
-// const passport = require('passport');
+const passport = require('passport');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(cookieParser());
-app.use(session({ secret: "nothing to see here" }));
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(session({ secret: "cats" }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-// require('./routes/passport')(app);
-require('./routes/account')(app);
-require('./routes/fbLogin')(app);
-require('./routes/email')(app);
+require('./routes/passport')(app);
 
-if (process.env.NODE_ENV ==='production'){
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/under-the-weather";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).catch(err=>console.log(err));
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
