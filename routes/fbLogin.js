0946b5ec5d29d60960859e53a4b47f3b1dadbbc6
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-10 14:35:34 -0700

    changed first axios get token to code

--- routes/fbLogin.js
+++ routes/fbLogin.js
@@ -6,15 +6,15 @@ const state = uid.sync(18);
 
 module.exports = (app) => {
     app.get('/auth/facebook', (req,res)=>{
-        res.redirect(`https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&state=${state}&response_type=code&scope=public_profile%20email`)
-        console.log('auth')
+        res.redirect(`https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&state=${state}&response_type=code&scope=public_profile%20email&display=popup`)
+        console.log(`https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&state=${state}&response_type=code&scope=public_profile%20email`)
     });
 
     app.get('/login/', (req,res)=>{
         const token = req.query.code
         const urlState = req.query.state
         if (urlState === state){
-            axios.get(`https://graph.facebook.com/v4.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&client_secret=${process.env.FACEBOOK_APP_SECRET}&token=${token}`)
+            axios.get(`https://graph.facebook.com/v4.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${token}`)
                 .then(code=>{
                     console.log(code.data)
                     axios.get(`https://graph.facebook.com/debug_token?input_token=${code.data.access_token}&access_token=${process.env.FACEBOOK_APP_TOKEN}`)
