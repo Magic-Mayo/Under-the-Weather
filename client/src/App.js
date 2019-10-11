970d8ab689559321f6f3e0dc9f90cd089c3064bc
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-10-10 19:40:37 -0700

    able to change the state and get lat and long of users location

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,9 @@ const facebook = () => {
 function App() {
 
   return (
-    <div className="App">
-      <Test />
+    <div className="App">      
         <div className="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true" onClick={facebook}>Facebook login</div>         
+      <Test />
     </div>
   );
 }
--- client/src/components/apiTest.js
+++ client/src/components/apiTest.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import API from "../utils/API"
+import React, {Component} from 'react';
+// import API from "../utils/API"
+// import Constants from 'expo-constants';
+// import * as Location from 'expo-location';
+// import * as Permissions from 'expo-permissions';
 
-function Test() {
+class Test extends Component{
+    state ={
+        longitude: "aaa",
+        latitude: "bbb"
 
+    }
+   
+   componentDidMount(){
+       this.findLocation()
+    //    .then(res => (this.setState({longitude: res.crd.longitude, latitude: res.crd.latitude})));
+   }
+   
     
     // API.defaultSearch().then(res => (console.log(res.data.data)));
     // API.SearchLocation("az-mesa").then(res => (console.log(res)));
-    API.SearchSpecialty("pediatrician", "az-mesa").then(res => (console.log(res.data.data)))
-;    
-    return (
-        <div></div>
-    )
+    // API.SearchSpecialty("pediatrician", "az-mesa").then(res => (console.log(res.data.data)));
 
+    findLocation = (pos, err) =>{
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      },
+      
+      success =(pos) => {
+        const crd = pos.coords;
+       
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+        this.setState({longitude: crd.longitude, latitude: crd.latitude})
+             
+      
+        },
+      
+      error = (err) =>{
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+      
+      navigator.geolocation.getCurrentPosition(success, error, options); 
+      
+    };    
+
+render(){
+
+    return (
+        <div>
+            <h1>
+                {this.state.latitude}
+                <span>   </span> 
+                {this.state.longitude}
+            </h1>
+        </div>
+        )
+        
+    }
 }
 
 export default Test;
