390b3c819ea8d8026129dbe8d929130bbcf6ffba
MagicMayo <mike.mayoii@gmail.com>
2019-10-13 11:55:24 -0700

    added db query to add user if not found on fb authentication

--- routes/fbLogin.js
+++ routes/fbLogin.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const uid = require('uid-safe');
 const axios = require('axios');
-
+const controller = require('../controllers');
+const db = require('../models');
 const state = uid.sync(18);
 
 module.exports = (app) => {
     app.get('/auth/facebook', (req,res)=>{
         res.redirect(`https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&state=${state}&response_type=code&scope=public_profile%20email&display=popup`)
-        console.log(`https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&state=${state}&response_type=code&scope=public_profile%20email`)
     });
 
     app.get('/login/', (req,res)=>{
@@ -16,18 +16,29 @@ module.exports = (app) => {
         if (urlState === state){
             axios.get(`https://graph.facebook.com/v4.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${token}`)
                 .then(code=>{
-                    console.log(code.data)
                     axios.get(`https://graph.facebook.com/debug_token?input_token=${code.data.access_token}&access_token=${process.env.FACEBOOK_APP_TOKEN}`)
                         .then(debuggedToken=>{
-                            console.log(debuggedToken)
                             axios.get(`https://graph.facebook.com/${debuggedToken.data.data.user_id}?fields=id,email,name&access_token=${code.data.access_token}`)
                                 .then(userData=>{
-                                    console.log(userData)
+                                    db.User.findOne({where: {userName: userData.id}}).then(user=>{
+                                        if(!user){
+                                            console.log(user)
+                                            db.User.create({
+                                                userName: userData.id,
+                                                name: userData.name,
+                                                email: userData.email
+                                            }).then(newUser=>{
+                                                console.log(newUser)
+                                                // res.redirect(`/newaccount/${newUser.userName}`)
+                                            }).catch(err=>console.log(err))
+                                        }
+                                        res.redirect(`/home/${userData.id}`)
+                                    }).catch(err=>console.log(err))
                                 })
                         })
                         .catch(err=>console.log(err))
                 })
                 .catch(err=>console.log(err))
         }
-    });    
+    });
 }
