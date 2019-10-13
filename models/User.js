0f7164cb43af332f9ca1c20007c89fd3d8036137
MagicMayo <mike.mayoii@gmail.com>
2019-10-13 11:55:23 -0700

    removed validation on userName field

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    userName: {type: String, required: [true, 'User name is required'], unique: true},
+    userName: {type: String, unique: true},
     password: {type: String, default: ''},
     name: {type: String, default: ''},
     email: {type: String, default: ''},
