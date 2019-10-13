4bf08a3d7124c3fcba81a98913cc05c894364d57
MagicMayo <mike.mayoii@gmail.com>
2019-10-13 11:55:23 -0700

    changed all to console log for testing

--- controllers/index.js
+++ controllers/index.js
@@ -3,17 +3,23 @@ const db = require('../models');
 module.exports = {
     findById: (req,res)=>{
         db.User.findById(req.params.user)
-            .then(user=>res.json(user)
-            .catch(err=>res.json(err)))
+            .then(user=>console.log(user)
+            .catch(err=>console.log(err)))
     },
-    update: (req,res)=>{
+    updateAccount: (req,res)=>{
         db.User.findOneAndUpdate({userName: req.params.user},req.body)
-            .then(updated=>res.json(updated))
-            .catch(err=>res.json(err))
+            .then(updated=>console.log(updated))
+            .catch(err=>console.log(err))
     },
     createUser: (req,res)=>{
         db.User.create(req.body)
-            .then(newUser=>res.json(newUser))
-            .catch(err=>res.json(err))
-    }
+            .then(newUser=>console.log(newUser))
+            .catch(err=>console.log(err))
+    },
+    logSymptom: (req,res)=>{
+        db.User.findOneAndUpdate({userName: req.params.user},req.body)
+            .then(user=>{console.log(user)})
+            .catch(err=>console.log(err))
+    },
+
 }
