3dfdbe53040eee2020af9c99cc85553e8cf8ccc9
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-14 11:24:05 -0700

    started route for local login

--- routes/localLogin.js
+++ routes/localLogin.js
@@ -1,12 +1,31 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const db = require('../models')
 
-const signIn = new LocalStrategy({
-    
-})
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        db.User.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }
+));
 
 module.exports = (app) => {
-    app.post('/local-login', (req,res)=>{
-
+    app.post('/newlocal', (req,res)=>{
+        console.log(req.body)
     })
+
+    app.post('/login',
+        passport.authenticate('local'),
+        (req,res)=>{
+            res.redirect(`home/${req.user.username}`)
+        }
+    );
 }
