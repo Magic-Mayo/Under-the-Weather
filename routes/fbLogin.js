573eb7ee2eaf81b29b03000b2a57ed5ffe391924
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-14 11:24:05 -0700

    added axios get route when user is created

--- routes/fbLogin.js
+++ routes/fbLogin.js
@@ -10,35 +10,63 @@ module.exports = (app) => {
         res.redirect(`https://www.facebook.com/v4.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&state=${state}&response_type=code&scope=public_profile%20email&display=popup`)
     });
 
-    app.get('/login/', (req,res)=>{
+    app.get('/auth/facebook/login', (req,res)=>{
         const token = req.query.code
         const urlState = req.query.state
         if (urlState === state){
-            axios.get(`https://graph.facebook.com/v4.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${token}`)
-                .then(code=>{
-                    axios.get(`https://graph.facebook.com/debug_token?input_token=${code.data.access_token}&access_token=${process.env.FACEBOOK_APP_TOKEN}`)
-                        .then(debuggedToken=>{
-                            axios.get(`https://graph.facebook.com/${debuggedToken.data.data.user_id}?fields=id,email,name&access_token=${code.data.access_token}`)
-                                .then(userData=>{
-                                    db.User.findOne({where: {userName: userData.id}}).then(user=>{
-                                        if(!user){
-                                            console.log(user)
-                                            db.User.create({
-                                                userName: userData.id,
-                                                name: userData.name,
-                                                email: userData.email
-                                            }).then(newUser=>{
-                                                console.log(newUser)
-                                                // res.redirect(`/newaccount/${newUser.userName}`)
-                                            }).catch(err=>console.log(err))
-                                        }
-                                        res.redirect(`/home/${userData.id}`)
-                                    }).catch(err=>console.log(err))
-                                })
-                        })
-                        .catch(err=>console.log(err))
-                })
-                .catch(err=>console.log(err))
+            axios.get(`https://graph.facebook.com/v4.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${token}`).then(code=>{
+
+                axios.get(`https://graph.facebook.com/debug_token?input_token=${code.data.access_token}&access_token=${process.env.FACEBOOK_APP_TOKEN}`).then(debuggedToken=>{
+
+                    axios.get(`https://graph.facebook.com/${debuggedToken.data.data.user_id}?fields=id,email,name&access_token=${code.data.access_token}`).then(userData=>{
+
+                        db.User.findOne({userName: userData.data.id}).then(user=>{
+                            console.log(user)
+                            if(!user){
+                                db.User.create({
+                                    userName: userData.data.id,
+                                    name: userData.data.name,
+                                    email: userData.data.email
+                                }).then(newUser=>{
+                                    axios.get(`http://localhost:3000/new/email/${newUser.userName}`)
+                                }).catch(err=>console.log(err))
+                            } else {
+                                res.redirect(`http://localhost:3000/home/${user.userName}`)
+                            }
+                        }).catch(err=>console.log(err))
+                    }).catch(err=>console.log(err))
+                }).catch(err=>console.log(err))
+            }).catch(err=>console.log(err))
         }
     });
+
+    // app.get('/new/email/:user', (req,res) => {
+    //     const id = req.params.user.split('#')[0];
+    //     console.log(req)
+    //     const transport = mail.createTransport({
+    //         service: 'gmail',
+    //         auth: {
+    //             user: process.env.EMAIL_ADDRESS,
+    //             pass: process.env.EMAIL_PASS
+    //         }
+    //     })
+    //     db.findOne({userName: id}).then(user=>{
+    //         const message = {
+    //         from: process.env.EMAIL_ADDRESS,
+    //         to: user.email,
+    //         subject: 'Under the Weather: E-mail verification',
+    //         text: `Please follow the link to verify your email address. If the link does not work please copy and paste the following into your browser to verify your email address: https://under-the-weather.herokuapp.com/verify/email/${user.id}`,
+    //         html: `<p>Please follow the <a href='https://under-the-weather.herokuapp.com/verify/email/${user.id}'>link</a> to verify your email address. If the link does not work please copy and paste the following into your browser to verify your email address: https://under-the-weather.herokuapp.com/verify/email/${user.id}</p>`
+    //         };
+
+    //         transport.sendMail(message, (err,res)=>{
+    //             if(err){
+    //                 console.log(err)
+    //             } else {
+    //                 console.log(res)
+    //                 // res.redirect('/')
+    //             }
+    //         })
+    //     })
+    // })
 }
