73073d67bc7ec6112d76de03d4b2c95d53c5970b
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-14 11:24:05 -0700

    changed post to get on new email route

--- routes/email.js
+++ routes/email.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const mail = require('nodemailer');
-
+const db = require('../models');
 
 module.exports = (app) => {
-    app.get('/verify/email', (req,res) => {
-        const user = {
-            id: 65146484
-        }
+    app.get('/new/email/:user', (req,res) => {
+        const id = req.params.user.split('#')[0];
+        console.log(id)
         const transport = mail.createTransport({
             service: 'gmail',
             auth: {
@@ -14,22 +13,23 @@ module.exports = (app) => {
                 pass: process.env.EMAIL_PASS
             }
         })
-        // db.findById().then(user=>{
+        db.User.findOne({userName: id}).then(user=>{
             const message = {
-            from: 'undertheweather2019@gmail.com',
-            to: user.email || 'mayom15@hotmail.com',
-            subject: 'DO NOT REPLY: E-mail verification',
+            from: process.env.EMAIL_ADDRESS,
+            to: user.email,
+            subject: 'Under the Weather: E-mail verification',
             text: `Please follow the link to verify your email address. If the link does not work please copy and paste the following into your browser to verify your email address: https://under-the-weather.herokuapp.com/verify/email/${user.id}`,
             html: `<p>Please follow the <a href='https://under-the-weather.herokuapp.com/verify/email/${user.id}'>link</a> to verify your email address. If the link does not work please copy and paste the following into your browser to verify your email address: https://under-the-weather.herokuapp.com/verify/email/${user.id}</p>`
             };
-        // })
-        transport.sendMail(message, (err,res)=>{
-            if(err){
-                console.log(err)
-            }else{
-                console.log(res)
-            }
+
+            transport.sendMail(message, (err,res)=>{
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log(res)
+                    // res.redirect('/')
+                }
+            })
         })
-        res.redirect('/')
     })
 }
