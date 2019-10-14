88c1cbb2f5b574c02e9c5778605c04536364428a
Sean Alexander <1996.sean.alexander@gmail.com>
2019-10-14 14:00:59 -0700

    added conditional rendering for user logged in status

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,31 +1,53 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const NavItems = ({ show }) => {
+  return (
+    <div
+      className="expand-items-container"
+      style={{ visibility: show ? "visible" : "hidden" }}
+    >
+      <a href="/" className="expand-items">
+        provider
+      </a>
+      <a href="/" className="expand-items">
+        contact
+      </a>
+      <a href="/" className="expand-items">
+        symptoms
+      </a>
+      <a href="/" className="expand-items">
+        insurance
+      </a>
+    </div>
+  );
+};
 
-function Nav(props) {
-    function toggleMenu() {
-        props.menu.isExpanded = !props.menu.isExpanded;
-        console.log(props.menu.isExpanded)
-        this.setState(
-            {isExpanded: props.menu.isExpanded}
-        )
-    }
-    let rotated = props.menu.isExpanded ? "expand rotate" : "expand"
+const NavExpand = ({ onClick }) => {
+  return (
+    <div className="expand-container" onClick={onClick}>
+      <FontAwesomeIcon icon="plus" />
+    </div>
+  );
+};
 
+class Nav extends React.Component {
+  state = {
+    navItemsVisible: false
+  };
+  toggleMenu = () =>
+    this.setState(state => ({
+      dropDownVisible: !state.dropDownVisible
+    }));
+
+  render() {
     return (
-
-        <div className="Nav">
-            <div className="expand-container" onClick={toggleMenu}>
-                <FontAwesomeIcon icon="plus" className={rotated} />
-            </div>
-            <div className="expand-items-container">
-                <a href="/" className="expand-items">provider</a>
-                <a href="/" className="expand-items">contact</a>
-                <a href="/" className="expand-items">symptoms</a>
-                <a href="/" className="expand-items">insurance</a>
-            </div> 
-        </div>
-    )
+      <div className="Nav">
+        <NavExpand onClick={this.toggleMenu} />
+        <NavItems show={this.state.dropDownVisible} />
+      </div>
+    );
+  }
 }
 
-export default Nav
+export default Nav;
