fe6868181b8a660fb9168d7f57a76dbf2e237d73
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-15 15:18:09 -0700

    merge with sean branch

--- client/src/App.js
+++ client/src/App.js
@@ -3,45 +3,72 @@ import Header from './components/Header';
 import Dashboard from './components/pages/Dashboard'
 import LogInSignUp from './components/pages/LogInSignUp'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAngleDown, faPlus, faEnvelope, faPhone, faFilter, faSortDown } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faAngleDown,
+  faPlus,
+  faEnvelope,
+  faPhone,
+  faFilter,
+  faSortDown
+} from "@fortawesome/free-solid-svg-icons";
 
-import bodyParts from './data/bodyParts.json'
+import bodyParts from "./data/bodyParts.json";
 
-import './App.scss';
+import "./App.scss";
 
-library.add(faAngleDown, faPlus, faEnvelope, faPhone, faFilter, faSortDown)
+library.add(faAngleDown, faPlus, faEnvelope, faPhone, faFilter, faSortDown);
 
 function Main(props) {
-      const isLoggedIn = props.isLoggedIn
+  const isLoggedIn = props.isLoggedIn;
 
-      if (isLoggedIn) {
-        return <Dashboard state={props.state} />
-      } else {
-        return <LogInSignUp state={props.state} />
-      }
+  if (isLoggedIn) {
+    return <Dashboard state={props.state} />;
+  } else {
+    return <LogInSignUp state={props.state} />;
+  }
 }
 
- class App extends Component {
-   state = {
-     bodyParts,
 
-     menu: {
-       isExpanded: false
-     },
 
-     user: {
-       isLoggedIn: false
-     }
-   }
+class App extends Component {
+  state = {
+    bodyParts,
+
+    menu: {
+      isExpanded: false
+    },
+    isLoggedIn: false
+  };
+
+  handleHTTP = props => {
+    console.log("THIS IS THE LOGIN STATUS BEFORE SIGNING IN", props);
+
+    this.switchLoggedIn(props)
+
+    console.log("THIS IS THE LOGIN STATUS AFTER SIGNING IN", this.state.isLoggedIn);
+
+  };
+
+  switchLoggedIn = (logged) => {
+
+    this.setState({isLoggedIn: !logged});
+  }
+
+
 
   render() {
-    console.log(this.state.bodyParts);
+    // console.log(this.state.bodyParts);
 
     return (
       <div className="App">
+<<<<<<< HEAD
         <Header name="Sean" isLoggedIn={this.state.user.isLoggedIn}/>
         <Main state={this.state} isLoggedIn={this.state.user.isLoggedIn}/>
+=======
+        <Header name="Sean" isLoggedIn={this.state.isLoggedIn} handleHTTP={this.handleHTTP}/>
+        <Main state={this.state} isLoggedIn={this.state.isLoggedIn} />
+>>>>>>> sean
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
-const session = require("express-session");
+// const session = require("express-session");
 const bodyParser = require("body-parser");
 const passport = require('passport');
 const path = require('path');
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({ secret: "cats" }));
+// app.use(session({ secret: "cats" }));
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -28,8 +28,8 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/under-the-weather";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/under-the-weather";
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
