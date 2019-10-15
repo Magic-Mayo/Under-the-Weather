6d722f378afac002c2e3263767fe4c9ce1ebd655
Sean Alexander <1996.sean.alexander@gmail.com>
2019-10-15 14:48:26 -0700

    added log out redirect for front end (toggling state)

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,15 +3,19 @@ import Umbrella from "../icons/Umbrella";
 import Logo from "../icons/Logo";
 
 const Greeting = (props) => <h1 className="header-welcome">{props.isLoggedIn ? `Welcome Back, ${props.name}` : null}</h1>;
-const LoginStatus = (props) => <a href={props.href} className="header-status">{props.isLoggedIn ? "Sign Out" : "Log In"}</a>;
+const LoginStatus = (props) => {
+  console.log(props.isLoggedIn)
+  return props.isLoggedIn && <a className="header-status" onClick={props.onClick}>Sign Out</a>
+}; 
 
 
 export default class Header extends Component {
 
+
   render() {
     return (
       <div className="header">
-        <LoginStatus isLoggedIn={this.props.isLoggedIn} href={this.props.isLoggedIn ? "/loggedin" : "/signup"}/>
+        <LoginStatus isLoggedIn={this.props.isLoggedIn} href="/" onClick={() => this.props.handleHTTP(this.props.isLoggedIn)}/>
         <Umbrella width={"100%"} height={"100%"} />
         <Logo width={"100%"} height={"100%"} />
         <Greeting isLoggedIn={this.props.isLoggedIn} name={this.props.name} />
@@ -19,3 +23,13 @@ export default class Header extends Component {
     );
   }
 }
+
+
+
+// PSUEDO CODE FOR SIGNING IN LINK
+
+// renders based off of login status
+// if user clicks <a></a>, sends a request to server to log in/out w/ local db & facebook & google
+// server receives boolean representing log in stat 
+//  changes what needs to be changed => PROMISE => send boolean back to change state.user.isLoggedIn
+
