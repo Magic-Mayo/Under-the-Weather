fb11266e7ceba93b3014fc6177b492c643c7c0fe
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-16 13:40:22 -0700

    made anchor for testing fb login

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -4,18 +4,15 @@ import Logo from "../icons/Logo";
 
 const Greeting = (props) => <h1 className="header-welcome">{props.isLoggedIn ? `Welcome Back, ${props.name}` : null}</h1>;
 const LoginStatus = (props) => {
-  console.log(props.isLoggedIn)
-  return props.isLoggedIn && <a className="header-status" onClick={props.onClick}>Sign Out</a>
+  return !props.isLoggedIn && <a className="header-status" href='http://localhost:3001/auth/facebook' onClick={props.onClick}>Sign Out</a>
 }; 
 
-
 export default class Header extends Component {
-
 
   render() {
     return (
       <div className="header">
-        <LoginStatus isLoggedIn={this.props.isLoggedIn} href="/" onClick={() => this.props.handleHTTP(this.props.isLoggedIn)}/>
+        <LoginStatus isLoggedIn={this.props.isLoggedIn} onClick={this.props.handleHTTP}/>
         <Umbrella width={"100%"} height={"100%"} />
         <Logo width={"100%"} height={"100%"} />
         <Greeting isLoggedIn={this.props.isLoggedIn} name={this.props.name} />
