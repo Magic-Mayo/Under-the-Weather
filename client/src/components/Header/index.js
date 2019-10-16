e7dba74f7456b3fa32a94bf4314220269b96fa10
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-16 15:11:27 -0700

    removed loginstatus component and moved it to app.js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,17 +2,13 @@ import React, { Component } from "react";
 import Umbrella from "../icons/Umbrella";
 import Logo from "../icons/Logo";
 
-const Greeting = (props) => <h1 className="header-welcome">{props.isLoggedIn ? `Welcome Back, ${props.name}` : null}</h1>;
-const LoginStatus = (props) => {
-  return !props.isLoggedIn && <a className="header-status" href='http://localhost:3001/auth/facebook' onClick={props.onClick}>Sign Out</a>
-}; 
+const Greeting = (props) => <h1 className="header-welcome">{props.isLoggedIn ? `Welcome Back, ${props.name}` : null}</h1>; 
 
 export default class Header extends Component {
 
   render() {
     return (
       <div className="header">
-        <LoginStatus isLoggedIn={this.props.isLoggedIn} onClick={this.props.handleHTTP}/>
         <Umbrella width={"100%"} height={"100%"} />
         <Logo width={"100%"} height={"100%"} />
         <Greeting isLoggedIn={this.props.isLoggedIn} name={this.props.name} />
