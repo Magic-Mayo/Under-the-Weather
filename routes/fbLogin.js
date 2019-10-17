d533fc8f0a0b971c919b1432dea2098af5a28d05
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-17 11:59:27 -0700

    added bcrypt to hash token

--- routes/fbLogin.js
+++ routes/fbLogin.js
@@ -5,6 +5,7 @@ const controller = require('../controllers');
 const db = require('../models');
 const state = uid.sync(18);
 const moment = require('moment');
+const bcrypt = require('bcrypt');
 
 module.exports = (app) => {
     app.get('/auth/facebook', (req,res)=>{
@@ -14,7 +15,6 @@ module.exports = (app) => {
     app.get('/auth/facebook/login', (req,res)=>{
         const token = req.query.code
         const urlState = req.query.state
-        // res.redirect('/loading')
         if (urlState === state){
             axios.get(`https://graph.facebook.com/v4.0/oauth/access_token?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT}&client_secret=${process.env.FACEBOOK_APP_SECRET}&code=${token}`).then(code=>{
                 
@@ -23,27 +23,31 @@ module.exports = (app) => {
                     axios.get(`https://graph.facebook.com/${debuggedToken.data.data.user_id}?fields=id,email,name&access_token=${code.data.access_token}`).then(userData=>{
 
                         db.User.findOne({userName: userData.data.id}).then(user=>{
-                            console.log(user)
                             if(!user){
-                                db.User.create({
-                                    userName: userData.data.id,
-                                    name: userData.data.name,
-                                    email: userData.data.email,
-                                    token: code.data.access_token,
-                                    lastLogin: moment()
-                                }).then(newUser=>{
-                                    axios.get(`/new/email/${newUser.userName}`)
-                                    .then(res.redirect(`http://localhost:3000/dashboard/${newUser.userName}`))
-                                }).catch(err=>console.log(err))
+                                bcrypt.hash(code.data.access_token, 12).then((hash)=>{
+                                    db.User.create({
+                                        userName: userData.data.id,
+                                        name: userData.data.name,
+                                        email: userData.data.email,
+                                        token: hash,
+                                        socialMedia: true,
+                                        isLoggedIn: true,
+                                        lastLogin: moment()
+                                    }).then(newUser=>{
+                                        console.log(newUser)
+                                        axios.get(`/new/email/${newUser.userName}`)
+                                        .then(res.redirect(`http://localhost:3000/dashboard/${newUser._id}`))
+                                    }).catch(err=>console.log(`create new ${err}`))
+                                }).catch(err=>console.log(`bcrypt ${err}`))
                             }
-                            db.User.update({where: {userName: user.userName}, lastLogin: moment()})
+                            db.User.update({where: {userName: user.userName}, isLoggedIn: true, lastLogin: moment()})
                             .then(
-                                res.redirect(`http://localhost:3000/dashboard/${user.userName}`)
-                            )
-                        }).catch(err=>console.log(err))
-                    }).catch(err=>console.log(err))
-                }).catch(err=>console.log(err))
-            }).catch(err=>console.log(err))
+                                res.redirect(`http://localhost:3000/dashboard/${user._id}`)
+                            ).catch(err=>console.log(`update login${err}`))
+                        }).catch(err=>console.log(`find user ${err}`))
+                    }).catch(err=>console.log(`final fb axios ${err}`))
+                }).catch(err=>console.log(`second fb axios ${err}`))
+            }).catch(err=>console.log(`first fb axios ${err}`))
         }
     });
 }
