c49d60ee77fd71ce51078ca9148ecc524611a7e7
MagicMayo <mike.mayoii@gmail.com>
2019-10-18 00:06:31 -0700

    changed to functional component for testing and less complexity

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,31 +2,20 @@ import React, { Component } from "react";
 import Umbrella from "../icons/Umbrella";
 import Logo from "../icons/Logo";
 
-const Greeting = (props) => <h1 className="header-welcome">{props.isLoggedIn ? `Welcome Back, ${props.name}` : null}</h1>; 
-const LogOut = props => {
+const Greeting = (props) => props.isLoggedIn && <h1 className="header-welcome">Welcome Back, {props.name}</h1>; 
+
+const Header = props => {
     console.log(props)
-    return props.isLoggedIn && <button onClick={()=>props.logOut(props.user)}>Sign Out</button>
-}
-
-export default class Header extends Component {
-  render() {
     return (
-      <div className="header">
-        <LogOut logOut={this.props.handleLogOut} isLoggedIn={this.props.isLoggedIn} user={this.props.user}/>
-        <Umbrella width={"100%"} height={"100%"} />
-        <Logo width={"100%"} height={"100%"} />
-        <Greeting isLoggedIn={this.props.isLoggedIn} name={this.props.name} />
-      </div>
+        <div className="header">
+            {props.isLoggedIn && <button onClick={props.handleLogOut}>
+                Sign Out
+            </button>}
+            <Umbrella width={"100%"} height={"100%"} />
+            <Logo width={"100%"} height={"100%"} />
+            <Greeting isLoggedIn={props.isLoggedIn} name={props.name} />
+        </div>
     );
-  }
 }
 
-
-
-// PSUEDO CODE FOR SIGNING IN LINK
-
-// renders based off of login status
-// if user clicks <a></a>, sends a request to server to log in/out w/ local db & facebook & google
-// server receives boolean representing log in stat 
-//  changes what needs to be changed => PROMISE => send boolean back to change state.user.isLoggedIn
-
+export default Header;
