97ba282594b87e284e9de1d6c4006215a8fa6ca2
MagicMayo <mike.mayoii@gmail.com>
2019-10-18 00:05:43 -0700

    returned get in handleLogIn so it doesn't run next item in function. changed put in handleLogOut to include user id in url. conditionally rendered main or dashboard components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Dashboard from './components/pages/Dashboard'
 import LogInSignUp from './components/pages/LogInSignUp'
 import Loading from './components/icons/loading'
+// import Umbrella from "./components/icons/Umbrella";
+// import Logo from "./components/icons/Logo";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faAngleDown,
@@ -32,7 +34,7 @@ function Main(props) {
         return (
             <div>
                 <FacebookLogin loading={props.loading} onClick={props.onLoad}/>
-                <LogInSignUp loading={props.loading} handleLogIn={props.handleLogIn}/>
+                <LogInSignUp loading={props.loading} handleLogIn={props.handleLogIn} />
             </div>
         )
     }
@@ -52,13 +54,12 @@ class App extends Component {
     
     handleLogIn = props => {
         if (typeof props === 'object'){
-            Axios.post(`/login/${props.username}`, props.password)
+            return Axios.post(`/login/`, props)
             .then(user=>{
                 localStorage.setItem('_underweather', user.token);
-                return this.setState({loading: false, user: user.data, isLoggedIn: true})
+                this.setState({loading: false, user: user.data, isLoggedIn: true})
             })
-        }
-
+        } 
         Axios.get(`/user/${props}`).then(user=>{
             localStorage.setItem('_underweather', user.data.token);
             this.setState({loading: false, user: user.data, isLoggedIn: true});
@@ -69,29 +70,27 @@ class App extends Component {
         this.setState({loading: true})
     }
 
-    handleLogOut = user => {
+    handleLogOut = () => {
         this.setState({loading: true})
-        Axios.put(`/logout/${user}`, false).then(loggedOut=>{
+        console.log('clicked')
+        Axios.put(`/logout/${this.state.user._id}`, false).then(loggedOut=>{
+            localStorage.removeItem('_underweather')
             this.setState({isLoggedIn: loggedOut.data, user: '', loading: false});
             window.location.pathname = loggedOut.data.path
             console.log(loggedOut)
         })
     }
 
-    switchLoggedIn = (logged) => {
-
-    this.setState({isLoggedIn: !logged});
-    }
-
   render() {
     // console.log(this.state.bodyParts);
 
     return (
-      <div className="App">
-        <Header name={this.state.user.name} user={this.state.user._id} isLoggedIn={this.state.isLoggedIn} loading={this.state.loading} handleLogOut={this.handleLogOut}/>
-        <Main isLoggedIn={this.state.loading} onLoad={this.isLoading} handleLogIn={this.handleLogIn} loading={this.state.loading}/>
-        {this.state.isLoggedIn && <Dashboard {...this.state.user} menu={this.state.menu}/>}
-      </div>
+        <div className="App">
+            <Header name={this.state.user.name} user={this.state.user._id} isLoggedIn={this.state.isLoggedIn} loading={this.state.loading} handleLogOut={this.handleLogOut}/>
+            {!this.state.isLoggedIn ? 
+            <Main isLoggedIn={this.state.loading} onLoad={this.isLoading} handleLogIn={this.handleLogIn} loading={this.state.loading}/>:
+            <Dashboard {...this.state.user} menu={this.state.menu}/>}
+        </div>
     )
   }
 }
