43e01c68236468b7e9a9a6a3c401fda6862d7701
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-18 17:01:40 -0700

    condensed greeting function into Header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import Umbrella from "../icons/Umbrella";
 import Logo from "../icons/Logo";
 
-const Greeting = (props) => props.isLoggedIn && <h1 className="header-welcome">Welcome Back, {props.name}</h1>; 
-
 const Header = props => {
     return (
         <div className="header">
             {props.isLoggedIn && <button onClick={props.handleLogOut}>Sign Out</button>}
             <Umbrella width={"100%"} height={"100%"} />
             <Logo width={"100%"} height={"100%"} />
-            <Greeting isLoggedIn={props.isLoggedIn} name={props.name} />
+            {props.isLoggedIn && <h1 className="header-welcome">Welcome Back, {props.name}</h1>}
         </div>
     );
 }
