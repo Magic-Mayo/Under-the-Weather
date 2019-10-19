6caf2ff00ce771af36ad0767716b586b9d67a7d9
Sean A <1996.sean.alexander@gmail.com>
2019-10-18 21:54:15 -0700

    fleshed sign up form out - added 2nd page. 'sex/gender input needs to be dropdown select input if possible

--- client/src/components/forms/SignupForm.js
+++ client/src/components/forms/SignupForm.js
@@ -6,7 +6,15 @@ export default class SignupForm extends Component {
 	state = {
 		username: '',
 		password: '',
-		passwordCheck: ''
+		passwordCheck: '',
+		firstname: '',
+		lastname: '',
+		sex: {
+			male: false,
+			female: false
+		},
+		age: 0,
+		currentPage: 0
 	};
 
 	handleInput = (e) => {
@@ -14,67 +22,155 @@ export default class SignupForm extends Component {
 		this.setState({ [name]: value });
 	};
 
+	nextPage = (e) => {
+		console.log(this.state.currentPage);
+		this.setState({
+			currentPage: this.state.currentPage + 1
+		});
+	};
+
 	render() {
 		return (
 			<div className="grid">
 				<form className={`form-${this.props.data.loginType}-input-box form-input-area`}>
-                <h1 className="form-title">{this.props.data.headingText}</h1>
-					<div className="input-container">
-						<label htmlFor="username">
-							<span>*</span> Username:{' '}
-						</label>
-						<input
-							type="username"
-							name="username"
-							id="username"
-							value={this.state.username}
-							onChange={this.handleInput}
-							placeholder="johndoe24"
+					<h1 className="form-title">{this.props.data.headingText}</h1>
+					{this.state.currentPage === 0 ? (
+						<FirstPage
+							handleInput={this.handleInput}
+							username={this.state.username}
+							password={this.state.password}
+							showPassword={this.props.showPassword}
+							togglePassword={this.props.togglePassword}
+							passwordCheck={this.state.passwordCheck}
 						/>
-					</div>
-					<div className="input-container">
-						<label htmlFor="password">
-							<span>*</span> Password:{' '}
-						</label>
-						<input
-							type={this.props.showPassword ? 'text' : 'password'}
-							name="password"
-							value={this.state.password}
-							onChange={this.handleInput}
-							id="password"
+					) : this.state.currentPage === 1 ? (
+						<SecondPage
+							handleInput={this.handleInput}
+							firstname={this.state.firstname}
+							lastname={this.state.lastname}
+							sex={this.state.sex}
+							age={this.state.age}
 						/>
-						<FontAwesomeIcon
-							icon={this.props.showPassword ? 'eye-slash' : 'eye'}
-							className="eye-icon"
-							onClick={this.props.togglePassword}
-						/>
-					</div>
-					<div className="input-container">
-						<label htmlFor="password-check">
-							<span>*</span> Re-enter Password:{' '}
-						</label>
-						<input
-							type="password"
-							name="passwordCheck"
-							value={this.state.passwordCheck}
-							onChange={this.handleInput}
-							id="password-check"
-						/>
-					</div>
+					) : null}
 					<div className="btn-container">
-						<button type="button" className="btn">
+						<button type="button" className="btn" onClick={this.nextPage}>
 							Continue
 						</button>
 					</div>
 				</form>
-                <SignInSocial />
+				<SignInSocial />
 			</div>
 		);
 	}
 }
 
-function firstPage() {}
+const FirstPage = (props) => {
+	console.log(props);
+	return (
+		<span style={{ width: '100%' }}>
+			<div className="input-container">
+				<label htmlFor="username">
+					<span>*</span> Username:{' '}
+				</label>
+				<input
+					type="username"
+					name="username"
+					id="username"
+					value={props.username}
+					onChange={props.handleInput}
+					placeholder="johndoe24"
+				/>
+			</div>
+			<div className="input-container">
+				<label htmlFor="password">
+					<span>*</span> Password:{' '}
+				</label>
+				<input
+					type={props.showPassword ? 'text' : 'password'}
+					name="password"
+					value={props.password}
+					onChange={props.handleInput}
+					id="password"
+				/>
+				<FontAwesomeIcon
+					icon={props.showPassword ? 'eye-slash' : 'eye'}
+					className="eye-icon"
+					onClick={props.togglePassword}
+				/>
+			</div>
+			<div className="input-container">
+				<label htmlFor="password-check">
+					<span>*</span> Re-enter Password:{' '}
+				</label>
+				<input
+					type="password"
+					name="passwordCheck"
+					value={props.passwordCheck}
+					onChange={props.handleInput}
+					id="password-check"
+				/>
+			</div>
+		</span>
+	);
+};
 
-function secondPage() {}
+function SecondPage(props) {
+	return (
+		<span style={{ width: '100%' }} className="sign-up-second-page">
+			<div className="input-container">
+				<label htmlFor="firstname">
+					<span>*</span> First Name:{' '}
+				</label>
+				<input
+					type="text"
+					name="firstname"
+					id="firstname"
+					value={props.firstname}
+					onChange={props.handleInput}
+					placeholder="John"
+				/>
+			</div>
+			<div className="input-container">
+				<label htmlFor="lastname">
+					<span>*</span> Last Name:{' '}
+				</label>
+				<input 
+					type="text" 
+					name="lastname"
+					id="lastname"
+					value={props.lastname} 
+					onChange={props.handleInput}
+					placeholder="Doe"  />
+			</div>
+			<div className="flex-between">
+				<div className="input-container">
+					<label htmlFor="sex">
+						<span>*</span> Sex:{' '}
+					</label>
+					<input
+						type="text"
+						name="sex"
+						id="sex"
+						value={props.lastname}
+						onChange={props.handleInput}
+					/>
+				</div>
+				<div className="input-container">
+					<label htmlFor="age">
+						<span>*</span> Age:{' '}
+					</label>
+					<input
+						type="number"
+						name="age"
+						id="age"
+						value={props.age}
+						onChange={props.handleInput}
+						placeholder="36"
+					/>
+				</div>
+			</div>
+		</span>
+	);
+}
 
-function thirdPage() {}
+function ThirdPage() {}
