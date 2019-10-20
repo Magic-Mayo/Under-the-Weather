de0ccfab10c81069a1f1fc69af9da1ba6c2864b2
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-20 15:21:14 -0700

    changed structure so only non-sensitive data can be sent to front end

--- models/User.js
+++ models/User.js
@@ -2,59 +2,61 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    userName: {type: String, unique: true},
-    password: {type: String, default: ''},
-    token: String,
-    name: {type: String, default: ''},
-    email: {type: String, default: ''},
-    DOB: {type: Date, default: ''},
-    gender: {type: String, default: ''},
-    mediData: {
-        insurance: [{
-            provider: {type: String, default: ''},
-            type: {type: String, default: ''},
-            idNumber: {type: String, default: ''},
-            deductible: {type: String, default: ''},
-            copay: [{
-                doctor: Number,
-                specialist: Number,
-                urgentCare: Number,
-                emergency: Number,
-                prescription: {
-                    brandName: Number,
-                    generic: Number
-                }
+    data: {
+        userName: {type: String, unique: true},
+        name: {type: String, default: ''},
+        email: {type: String, default: ''},
+        DOB: {type: Date, default: ''},
+        gender: {type: String, default: ''},
+        mediData: {
+            insurance: [{
+                provider: {type: String, default: ''},
+                type: {type: String, default: ''},
+                idNumber: {type: String, default: ''},
+                deductible: {type: String, default: ''},
+                copay: [{
+                    doctor: Number,
+                    specialist: Number,
+                    urgentCare: Number,
+                    emergency: Number,
+                    prescription: {
+                        brandName: Number,
+                        generic: Number
+                    }
+                }],
+                createdAt: Date
             }],
+            doctors: [{
+                name: {type: String, default: ''},
+                type: {type: String, default: ''},
+                insuranceKey: {type: String, default: ''},
+                createdAt: Date
+            }]
+        },
+        emergencyContacts: [{
+            name: String,
+            phone: Number,
+            address: String,
+            relationship: String
+        }],
+        symptomHistory: [{
+            bodyPart: String,
+            painType: String,
+            severity: String,
+            symptoms: String,
+            time: Date,
+            diagnosis: String,
             createdAt: Date
         }],
-        doctors: [{
-            name: {type: String, default: ''},
-            type: {type: String, default: ''},
-            insuranceKey: {type: String, default: ''},
-            createdAt: Date
-        }]
+        isLoggedIn: Boolean,
     },
-    emergencyContacts: [{
-        name: String,
-        phone: Number,
-        address: String,
-        relationship: String
-    }],
-    symptomHistory: [{
-        bodyPart: String,
-        painType: String,
-        severity: String,
-        symptoms: String,
-        time: Date,
-        diagnosis: String,
-        createdAt: Date
-    }],
-    emailVerified: Boolean,
-    isLoggedIn: Boolean,
-    socialMedia: {type: Boolean, default: false},
     lastLogin: Date,
     createdAt: Date,
-    updatedAt: Date
+    updatedAt: Date,
+    socialMedia: {type: Boolean, default: false},
+    emailVerified: Boolean,
+    password: {type: String, default: ''},
+    loginToken: String
 });
 
 const User = mongoose.model('UserSchema', UserSchema);
