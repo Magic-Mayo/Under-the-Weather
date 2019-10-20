53256694acd49d3b493d0028a0ac043dee13552e
MagicMayo <mike.mayoii@gmail.com>
2019-10-19 17:21:49 -0700

    removed facebook login function as it is not needed any longer

--- client/src/App.js
+++ client/src/App.js
@@ -13,28 +13,37 @@ import {
   faPhone,
   faFilter,
   faSortDown,
-  faChild
+  faChild,
+  faEye
 } from "@fortawesome/free-solid-svg-icons";
 import bodyParts from "./data/bodyParts.json";
 import "./App.scss";
-import Axios from 'axios';
+import axios from 'axios';
 
-library.add(faAngleDown, faPlus, faEnvelope, faPhone, faFilter, faSortDown,fab);
+library.add(faAngleDown, faPlus, faEnvelope, faPhone, faFilter, faSortDown,fab, faEye);
 
-const FacebookLogin = props => {
-    return !props.isLoggedIn && <a className="header-status" href='http://localhost:3001/auth/facebook' onClick={props.onClick}>Sign In With Facebook</a>
-};
+const UTWtoken = localStorage.getItem('_underweather');
+if (UTWtoken){
+    axios.post('/token', {token: UTWtoken}).then(user=>{
+        console.log(user)
+        if(user){
+            return window.location.pathname = `dashboard/${user}`
+        }
+    }).catch(err=>console.log(err))
+}
 
 const Main = props => {
-    if(window.location.pathname.substring(1,10) === 'dashboard'){
+    console.log(props)
+    if(window.location.pathname.substring(1,10) === 'dashboard' && !props.isLoggedIn){
+        props.logIn();
         const user = window.location.pathname.split('board/')[1];
-        console.log(user)
         return <Loading path={user} loading={props.loading} onClick={props.handleLogIn} onLoad={props.onLoad}/>
     } else {
         return (
             <div>
-                <FacebookLogin loading={props.loading} onClick={props.onLoad}/>
-                <LogInSignUp loading={props.loading} handleLogIn={props.handleLogIn} />
+                {props.loading ? 
+                <Loading loading={props.loading} path={props.user} onLoad={props.onLoad} handleLogIn={props.handleLogIn}/>:
+                <LogInSignUp loading={props.loading} handleLogIn={props.handleLogIn}/>}
             </div>
         )
     }
@@ -49,19 +58,21 @@ class App extends Component {
         },
         isLoggedIn: false,
         loading: false,
-        user: ''
+        user: false
     };
     
     handleLogIn = props => {
         this.setState({loading: true})
         if (typeof props === 'object'){
-            return Axios.post(`/login`, props)
+            return axios.post(`/login`, props)
             .then(user=>{
                 this.setState({loading: false, user: user.data, isLoggedIn: true})
                 localStorage.setItem('_underweather', user.token);
             })
-        } 
-        Axios.get(`/user/${props}`).then(user=>{
+        }
+
+        axios.get(`/user/${props}`).then(user=>{
+            window.location.pathname = '/dashboard';
             localStorage.setItem('_underweather', user.data.token);
             this.setState({loading: false, user: user.data, isLoggedIn: true});
         })
@@ -71,10 +82,14 @@ class App extends Component {
         this.setState({loading: true})
     }
 
+    logIn = () => {
+        this.setState({isLoggedIn: true})
+    }
+
     handleLogOut = () => {
         this.setState({loading: true})
         console.log('clicked')
-        Axios.put(`/logout/${this.state.user._id}`, {loggedIn: 'logout'}).then(loggedOut=>{
+        axios.put(`/logout/${this.state.user._id}`, {loggedIn: 'logout'}).then(loggedOut=>{
             localStorage.removeItem('_underweather')
             this.setState({isLoggedIn: loggedOut.data, user: '', loading: false});
             window.location.pathname = loggedOut.data.path
@@ -82,15 +97,17 @@ class App extends Component {
         })
     }
 
-  render() {
-    return (
-        <div className="App">
-            <Header name={this.state.user.name} user={this.state.user._id} isLoggedIn={this.state.isLoggedIn} loading={this.state.loading} handleLogOut={this.handleLogOut}/>
-            {!this.state.isLoggedIn ? 
-            <Main isLoggedIn={this.state.loading} onLoad={this.isLoading} handleLogIn={this.handleLogIn} loading={this.state.loading}/>:
-            <Dashboard {...this.state.user} menu={this.state.menu}/>}
-        </div>
-    )
+    
+    render() {
+        console.log(this.state)
+        return (
+            <div className="App">
+                <Header name={this.state.user.name} user={this.state.user._id} isLoggedIn={this.state.isLoggedIn} loading={this.state.loading} handleLogOut={this.handleLogOut}/>
+                {!this.state.isLoggedIn && !this.state.user ? 
+                <Main isLoggedIn={this.state.loading} logIn={this.logIn} onLoad={this.isLoading} handleLogIn={this.handleLogIn} loading={this.state.loading}/>:
+                <Dashboard user={this.state.user} menu={this.state.menu}/>}
+            </div>
+        )
   }
 }
 
