4f56c1847ae0594be61830ffa843cdeb7e4eccc8
Mike Mayo <mike.mayoii@gmail.com>
2019-10-22 02:04:41 -0700

    Mike (#18)
    * removed console log
    
    * added href to facebook login
    
    * changed type of password check to password
    
    * updated packages
    
    * removed passport packages and added connect-mongo
    
    * started adding route for google sign in
    
    * changed req.body to reflect incoming data
    
    * added elements to support google sign in
    
    * uncommented session to start setting up sessions
    
    * condensed greeting function into Header component
    
    * removed facebook login function as it is not needed any longer
    
    * changed name of login prop
    
    * started route for checking tokens
    
    * moved in conditional for window location to a component did mount function inside the class component
    
    * used component will mount to check tokens before mounting entrance form so user can be routed to dashboard if token is present in browser
    
    * removed onLoad function and changed name of prop being passed in
    
    * added props to entrance form and loading components
    
    * changed response to only sent non-sensitive info to front end
    
    * changed structure so only non-sensitive data can be sent to front end
    
    * changed structure of creating facebook data since User model changed
    
    * added upsert option to create new token on new login
    
    * removed Main function and incorporated it into componentDidMount. used history.pushstate to change window location pathname in handleLogin
    
    * removed component from import
    
    * changed didMount function to have correct dot notation when checking if a token exists
    
    * added href to google anchor tag
    
    * removed didMount lifecycle function
    
    * removed unneeded props from loading component
    
    * fixed dot notation in looking for if user is currently logged in and changed what is being sent to the front end if user is logging in
    
    * moved where userName and loginToken are stored
    
    * changed dot notation for getting email address from user document
    
    * fixed how new user is created since database structure changed and added in a token to send to the front end
    
    * changed data being sent to front end and changed token route to either send back false or user id
    
    * changed structure of dummy account
    
    * changed updateAccount to return new data after update
    
    * changed insurance type to insuranceType to avoid it being converted to a string
    
    * deleted unneeded files
    
    * added / to window history on line 52
    
    * added required property to input fields
    
    * added required property to inputs. made functions to validate password, check if user already exists, and sending info to server when submitted
    
    * made function to check database for users
    
    * made route to check for users
    
    * uncommented lines for using bcrypt on login
    
    * encrypted dummy password

--- routes/localLogin.js
+++ routes/localLogin.js
@@ -2,33 +2,46 @@ const db = require('../models');
 const bcrypt = require('bcrypt');
 const uid = require('uid-safe');
 const token = uid.sync(24);
+const controller = require('../controllers')
+const mongoose = require('mongoose');
+
+// const connection = 
 
 module.exports = (app) => {
+
+    app.get('/check', controller.findByName)
+
     app.post('/newlocal', (req,res)=>{
-        bcrypt.hash(req.body.credentials.password, 12)
-        .then(hash=>{
-            db.User.create({
-                userName: req.body.credentials.username,
-                password: hash,
-                token: token
+        console.log(req.body)
+        if(!req.body.username || !req.body.password){ return }
+
+        db.User.findOne({userName: req.body.username}).then(user=>{
+            if(user){ return }
+            bcrypt.hash(req.body.password, 12)
+            .then(hash=>{
+                db.User.create({
+                    userName: req.body.username,
+                    password: hash,
+                    token: token
+                })
+                .then(user=>{
+                    res.json(user)
+                })
+                .catch(err=>console.log(`Error: ${err}`))
             })
-            .then(user=>{
-                res.json(user)
-            })
-            .catch(err=>res.json(`Error: ${err}`))
         })
     })
 
     app.post('/login', (req,res)=>{
         const credentials = req.body.credentials;
         db.User.findOne({userName: credentials.username}).then(user=>{
-            // bcrypt.compare(credentials.password, user.password).then(verified=>{
-                if (user.password === credentials.password){
+            bcrypt.compare(credentials.password, user.password).then(verified=>{
+                if(verified){
                     return res.json({userId: user._id, user: user.data, userName: user.userName})
                 }
                 res.json('Incorrect username and password combination')
             }).catch(err=>console.log(err))
-        // }).catch(err=>console.log(err))
+        }).catch(err=>console.log(err))
     });
 
     app.post('/token/', (req,res)=>{
