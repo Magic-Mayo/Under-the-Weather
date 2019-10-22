155b340e6c4bd418856e1be9643967b7396fc37d
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-22 01:00:49 -0700

    made route to check for users

--- routes/localLogin.js
+++ routes/localLogin.js
@@ -2,20 +2,33 @@ const db = require('../models');
 const bcrypt = require('bcrypt');
 const uid = require('uid-safe');
 const token = uid.sync(24);
+const controller = require('../controllers')
+const mongoose = require('mongoose');
+
+// const connection = 
 
 module.exports = (app) => {
+
+    app.get('/check', controller.findByName)
+
     app.post('/newlocal', (req,res)=>{
-        bcrypt.hash(req.body.credentials.password, 12)
-        .then(hash=>{
-            db.User.create({
-                userName: req.body.credentials.username,
-                password: hash,
-                token: token
+        console.log(req.body)
+        if(!req.body.username || !req.body.password){ return }
+
+        db.User.findOne({userName: req.body.username}).then(user=>{
+            if(user){ return }
+            bcrypt.hash(req.body.password, 12)
+            .then(hash=>{
+                db.User.create({
+                    userName: req.body.username,
+                    password: hash,
+                    token: token
+                })
+                .then(user=>{
+                    res.json(user)
+                })
+                .catch(err=>console.log(`Error: ${err}`))
             })
-            .then(user=>{
-                res.json(user)
-            })
-            .catch(err=>res.json(`Error: ${err}`))
         })
     })
 
@@ -23,6 +36,7 @@ module.exports = (app) => {
         const credentials = req.body.credentials;
         db.User.findOne({userName: credentials.username}).then(user=>{
             // bcrypt.compare(credentials.password, user.password).then(verified=>{
+                // if(verified){
                 if (user.password === credentials.password){
                     return res.json({userId: user._id, user: user.data, userName: user.userName})
                 }
