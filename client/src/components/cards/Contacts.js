8ec7c0df975ab454b7e2d880020c27d99e476aa7
Zeb <millerZebadiah@gmail.com>
2019-10-23 16:35:24 -0700

    mapping emergency contacts needs styling

--- client/src/components/cards/Contacts.js
+++ client/src/components/cards/Contacts.js
@@ -4,33 +4,36 @@ export default class ContactsCard extends Component {
     state = {
         userContacts: []
     }
-  
+
     render() {
         return (
-            <div>
-                <section>
-                    {this.props.user.emergencyContacts.map((res) => (
-                        <Contacts
-                            name={res.name}
-                        />
+            <div className="Contacts card">
+            <h2>Emergency Contacts</h2>
+            {this.props.user.emergencyContacts.map((res) => (
+                <EmerContacts
+                    name={res.name}
+                    number={res.phone}
+                    address={res.address}
+                    relationship={res.relationship}
+                />
+            ))}
 
-                    ))};
-            </section>
-            </div>
+        </div>
 
         )
     };
 
 };
 
-function Contacts(props) {
+
+function EmerContacts(props) {
     return (
-        <div className="Contacts card">
-            <h2>Emergency Contacts</h2>
+        <div>
             <h4>Name: {props.name}</h4>
             <h4>Phone Number: {props.number}</h4>
             <h4>Address: {props.address}</h4>
             <h4>Relatonship: {props.relationship}</h4>
+            <br></br>
         </div>
     )
 }
