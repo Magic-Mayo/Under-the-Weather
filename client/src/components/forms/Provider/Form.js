649fce5a271438ec2f68cd3222849a3294ab148a
Sean A <1996.sean.alexander@gmail.com>
2019-10-22 20:01:22 -0700

    styled provider form

--- client/src/components/forms/Provider/Form.js
+++ client/src/components/forms/Provider/Form.js
@@ -34,11 +34,11 @@ export default class ProviderForm extends Component {
                     )
                 }
                 <div className="provider-form-submit-container">
-                    <button type="button" className="provider-form-submit">
+                    {/* <button type="button" className="provider-form-submit">
                         Add Provider
-                    </button>
+                    </button> */}
                     <button type="button" className="provider-form-close">
-                        CLose X
+                        Close Form X
                     </button>
                 </div>
 			</div>
