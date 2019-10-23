65297bdc5b3296ca9e131851b6b8b1ade9689943
MagicMayo <mike.mayoii@gmail.com>
2019-10-22 21:45:22 -0700

    started making controller for updating, creating, and deleting account data.  still need to finish up switch statement to direct traffic

--- controllers/index.js
+++ controllers/index.js
@@ -26,6 +26,103 @@ module.exports = {
                 res.json(true)
             })
     },
+    updateAccount: (req,res,next)=>{
+        let route;
+        switch(req.body.route){
+            case 'addprovider': route = {$push: {'data.mediData.doctors': req.body.provider}}; break;
+            case 'addcontact': route = {$push: {'data.mediData.doctors': req.body.contact}}; break;
+            case 'addsymptom': route = {$push: {'data.mediData.doctors': req.body.symptom}}; break;
+            case 'addinsurance': route = {$push: {'data.mediData.doctors': req.body.insurance}}; break;
+            case 'updateinsurance': route = 
+        }
+        if()
+        db.User.findOneAndUpdate({_id: req.body.userId}, route)
+            .then(provider=>{
+                console.log(provider)
+                if (provider){
+                    res.json('Provider added!')
+                }
+            })
+            .catch(err=>res.json('Error adding provider.  Please try again later.'));
+        next();
+    },
+    addSymptom: (req,res,next)=>{
+        db.User.findOneAndUpdate({_id: req.body.userId}, {$push: {'data.symptomHistory': req.body.symptom}})
+            .then(symptom=>{
+                console.log(symptom)
+                if (symptom){
+                    res.json('Symptom added!')
+                }
+            })
+            .catch(err=>res.json('Error adding symptom.  Please try again later.'));
+        next();
+    },
+    addContact: (req,res,next)=>{
+        db.User.findOneAndUpdate({_id: req.body.userId}, {$push: {'data.emergencyContacts': req.body.contact}})
+            .then(contact=>{
+                console.log(contact)
+                if (contact){
+                    res.json('Contact added!')
+                }
+            })
+            .catch(err=>res.json('Error adding contact.  Please try again later.'))
+        next();
+    },
+    addInsurance: (req,res,next)=>{
+        db.User.findOneAndUpdate({_id: req.body.userId}, {$push: {'data.mediData.insurance': req.body.insurance}})
+            .then(insurance=>{
+                console.log(insurance)
+                if (insurance){
+                    res.json('Insurance added!')
+                }
+            })
+            .catch(err=>res.json('Error adding Insurance.  Please try again later.'))
+        next();
+    },
+    updateInsurance: (req,res,next)=>{
+        db.User.findOneAndUpdate({_id: req.body.userId, 'data.mediData.insurance': req.body.insurance}, req.body.insurance, {new: true})
+            .then(insurance=>{
+                console.log(insurance)
+                if (insurance){
+                    res.json({msg: 'Insurance updated', update: insurance})
+                }
+            })
+            .catch(err=>res.json('Error updating insurance.  Please try again later.'))
+        next();
+    },
+    updateProvider: (req,res,next)=>{
+        db.User.findOneAndUpdate({_id: req.body.userId, 'data.mediData.doctors': req.body.insurance}, req.body.insurance, {new: true})
+            .then(insurance=>{
+                console.log(insurance)
+                if (insurance){
+                    res.json({msg: 'Insurance updated', update: insurance})
+                }
+            })
+            .catch(err=>res.json('Error updating insurance.  Please try again later.'))
+        next();
+    },
+    updateContact: (req,res,next)=>{
+        db.User.findOneAndUpdate({_id: req.body.userId, 'data.mediData.insurance': req.body.insurance}, req.body.insurance, {new: true})
+            .then(insurance=>{
+                console.log(insurance)
+                if (insurance){
+                    res.json({msg: 'Insurance updated', update: insurance})
+                }
+            })
+            .catch(err=>res.json('Error updating insurance.  Please try again later.'))
+        next();
+    },
+    updateSymptom: (req,res,next)=>{
+        db.User.findOneAndUpdate({_id: req.body.userId, 'data.symptomHistory': req.body.symptom}, req.body.newSymptom, {new: true})
+            .then(insurance=>{
+                console.log(insurance)
+                if (insurance){
+                    res.json({msg: 'Insurance updated', update: insurance})
+                }
+            })
+            .catch(err=>res.json('Error updating insurance.  Please try again later.'))
+        next();
+    },
     updateAccount: (req,res)=>{
         db.User.findOneAndUpdate({userName: req.params.user}, req.body, {new: true})
             .then(updated=>console.log(updated))
