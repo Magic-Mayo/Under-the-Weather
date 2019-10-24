144b2292795e39883ce7c81c98e50886f62838c8
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-24 00:44:52 -0700

    production build for testing

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    data:{
+        name: {type: String, default: ''},
+        email: {type: String, default: ''},
+        DOB: {type: Date, default: ''},
+        gender: {type: String, default: ''},
+        mediData: {
+            insurance: [{
+                provider: {type: String, default: ''},
+                insuranceType: {type: String, default: ''},
+                idNumber: {type: String, default: ''},
+                deductible: {type: String, default: ''},
+                copay: [{
+                    doctor: Number,
+                    specialist: Number,
+                    urgentCare: Number,
+                    emergency: Number,
+                    prescription: {
+                        brandName: Number,
+                        generic: Number
+                    }
+                }],
+                createdAt: Date
+            }],
+            doctors: [{
+                name: {type: String, default: ''},
+                type: {type: String, default: ''},
+                insuranceKey: {type: String, default: ''},
+                createdAt: Date
+            }]
+        },
+        emergencyContacts: [{
+            name: String,
+            phone: Number,
+            address: String,
+            relationship: String,
+            createdAt: String
+        }],
+        symptomHistory: [{
+            bodyPart: String,
+            painType: String,
+            severity: String,
+            symptoms: String,
+            time: Date,
+            diagnosis: String,
+            createdAt: Date
+        }],
+        isLoggedIn: Boolean,
+    },
+    userName: {type: String, unique: true},
+    loginToken: String,
+    lastLogin: Date,
+    createdAt: Date,
+    updatedAt: Date,
+    socialMedia: {type: Boolean, default: false},
+    emailVerified: Boolean,
+    password: {type: String, default: ''}
+});
+
+const User = mongoose.model('UserSchema', UserSchema);
+module.exports = User;
---
+++ scripts/dbSeeds.js
@@ -0,0 +1,125 @@
+const db = require('../models');
+const bcrypt = require('bcrypt')
+
+const dummy = {
+    data: {
+        name: 'Dummy McDumberson',
+        email: 'dummy@dum.dum',
+        DOB: Date.now(),
+        gender: 'Male',
+        mediData: {
+            insurance: [{
+                provider: 'Blue Cross Blue Shield',
+                type: 'Medical',
+                idNumber: '123456',
+                deductible: '5000/7500',
+                copay: [{
+                    doctor: 15,
+                    specialist: 30,
+                    urgentCare: 75,
+                    emergency: 200,
+                    prescription: {
+                        brandName: 15,
+                        generic: 2
+                    }
+                }],
+                createdAt: Date.now()
+            }],
+            doctors: [{
+                name: 'Dr. John Smith',
+                type: 'Podiatrist',
+                insuranceKey: '',
+                createdAt: Date.now()
+            },{
+                name: 'Dr. Pocahontas',
+                type: 'Pediatrician',
+                insuranceKey: '',
+                createdAt: Date.now()
+            }]
+        },
+        emergencyContacts: [{
+            name: 'Smart McDumberson',
+            phone: 3365555000,
+            address: '123 Oxymoron St Whynot, NC 27340',
+            relationship: 'Mother'
+        },{
+            name: 'Slightly Dumber McDumberson',
+            phone: 3365555000,
+            address: '123 Oxymoron St Whynot, NC 27340',
+            relationship: 'Father'
+        },{
+            name: 'Normal McNormal',
+            phone: 5205552365,
+            address: '456 Normal Rd Why, AZ 85789',
+            relationship: 'Sister'
+        }],
+        symptomHistory: [{
+            bodyPart: 'Head',
+            painType: 'Sharp',
+            severity: 'Mild',
+            symptoms: 'Headache',
+            time: Date.now(),
+            diagnosis: '',
+            createdAt: Date.now()
+        },{
+            bodyPart: 'Head',
+            painType: 'Pounding',
+            severity: 'Severe',
+            symptoms: 'Headache',
+            time: Date.now(),
+            diagnosis: '',
+            createdAt: Date.now()
+        },{
+            bodyPart: 'Stomach',
+            painType: 'Nauseous',
+            severity: 'Mild',
+            symptoms: 'Stomach ache',
+            time: Date.now(),
+            diagnosis: '',
+            createdAt: Date.now()
+        },{
+            bodyPart: 'Back',
+            painType: 'Aching',
+            severity: 'Mild',
+            symptoms: 'Back ache',
+            time: Date.now(),
+            diagnosis: '',
+            createdAt: Date.now()
+        },{
+            bodyPart: 'Head',
+            painType: 'Sharp',
+            severity: 'Mild',
+            symptoms: 'Headache',
+            time: Date.now(),
+            diagnosis: '',
+            createdAt: Date.now()
+        }],
+        isLoggedIn: true,
+    },
+    userName: 'dummy',
+    password: 'iisabigdumdum',
+    emailVerified: false,
+    socialMedia: false,
+    lastLogin: Date.now(),
+    createdAt: Date.now(),
+    updatedAt: Date.now()
+}
+
+
+
+module.exports = (app) => {
+    app.get('/dummydata', (req, res)=>{
+        bcrypt.hash(dummy.password, 12).then(pass=>{
+            dummy.password = pass;
+            db.User.findOne({userName: dummy.userName})
+                .then(user=>{
+                    console.log(user)
+                    if(!user){
+                        db.User.create(dummy)
+                            .then(dummy=>res.redirect(`http://localhost:3000/dashboard/${dummy._id}`))
+                            .catch(err=>console.log(err))
+                    }
+                })
+        })
+    })
+}
