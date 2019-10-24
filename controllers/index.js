826457284ce699f7d286e6a7ad141fb2c77ed552
Zeb <millerZebadiah@gmail.com>
2019-10-24 11:58:09 -0700

    catching up with dev

--- controllers/index.js
+++ controllers/index.js
@@ -26,92 +26,6 @@ module.exports = {
                 res.json(true)
             })
     },
-<<<<<<< HEAD
-    // updateAccount: (req,res,next)=>{
-    //     let route;
-    //     switch(req.body.route){
-    //         case 'addprovider': route = {$push: {'data.mediData.doctors': req.body.provider}}; break;
-    //         case 'addcontact': route = {$push: {'data.mediData.doctors': req.body.contact}}; break;
-    //         case 'addsymptom': route = {$push: {'data.mediData.doctors': req.body.symptom}}; break;
-    //         case 'addinsurance': route = {$push: {'data.mediData.doctors': req.body.insurance}}; break;
-    //         case 'updateinsurance': route = 
-    //     }
-    //     if()
-    //     db.User.findOneAndUpdate({_id: req.body.userId}, route)
-    //         .then(provider=>{
-    //             console.log(provider)
-    //             if (provider){
-    //                 res.json('Provider added!')
-    //             }
-    //         })
-    //         .catch(err=>res.json('Error adding provider.  Please try again later.'));
-    //     next();
-    // },
-    addSymptom: (req,res,next)=>{
-        db.User.findOneAndUpdate({_id: req.body.userId}, {$push: {'data.symptomHistory': req.body.symptom}})
-            .then(symptom=>{
-                console.log(symptom)
-                if (symptom){
-                    res.json('Symptom added!')
-                }
-            })
-            .catch(err=>res.json('Error adding symptom.  Please try again later.'));
-        next();
-    },
-    addContact: (req,res,next)=>{
-        db.User.findOneAndUpdate({_id: req.body.userId}, {$push: {'data.emergencyContacts': req.body.contact}})
-            .then(contact=>{
-                console.log(contact)
-                if (contact){
-                    res.json('Contact added!')
-                }
-            })
-            .catch(err=>res.json('Error adding contact.  Please try again later.'))
-        next();
-    },
-    addInsurance: (req,res,next)=>{
-        db.User.findOneAndUpdate({_id: req.body.userId}, {$push: {'data.mediData.insurance': req.body.insurance}})
-            .then(insurance=>{
-                console.log(insurance)
-                if (insurance){
-                    res.json('Insurance added!')
-                }
-            })
-            .catch(err=>res.json('Error adding Insurance.  Please try again later.'))
-        next();
-    },
-    updateInsurance: (req,res,next)=>{
-        db.User.findOneAndUpdate({_id: req.body.userId, 'data.mediData.insurance': req.body.insurance}, req.body.insurance, {new: true})
-            .then(insurance=>{
-                console.log(insurance)
-                if (insurance){
-                    res.json({msg: 'Insurance updated', update: insurance})
-                }
-            })
-            .catch(err=>res.json('Error updating insurance.  Please try again later.'))
-        next();
-    },
-    updateProvider: (req,res,next)=>{
-        db.User.findOneAndUpdate({_id: req.body.userId, 'data.mediData.doctors': req.body.insurance}, req.body.insurance, {new: true})
-            .then(insurance=>{
-                console.log(insurance)
-                if (insurance){
-                    res.json({msg: 'Insurance updated', update: insurance})
-                }
-            })
-            .catch(err=>res.json('Error updating insurance.  Please try again later.'))
-        next();
-    },
-    updateContact: (req,res,next)=>{
-        db.User.findOneAndUpdate({_id: req.body.userId, 'data.mediData.insurance': req.body.insurance}, req.body.insurance, {new: true})
-            .then(insurance=>{
-                console.log(insurance)
-                if (insurance){
-                    res.json({msg: 'Insurance updated', update: insurance})
-                }
-            })
-            .catch(err=>res.json('Error updating insurance.  Please try again later.'))
-=======
     updateAccount: (req,res,next)=>{
         const body = req.body;
         let route;
@@ -156,7 +70,6 @@ module.exports = {
                 })
                 .catch(err=>res.json('Error removing data.  Please try again later.'));
         }
->>>>>>> 3c0da9e2a241c43211df0f652a8dc877c05c8de7
         next();
     },
     updateProfile: (req,res,next)=>{
