0a32219a14165fe5eb74750de1702f904dc33172
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-25 18:38:03 -0700

    changed delete routes to accept params

--- routes/account.js
+++ routes/account.js
@@ -3,19 +3,19 @@ const db = require('../controllers/index');
 module.exports = (app) => {
     app.post('/account/insurance', db.updateAccount);
     app.put('/account/insurance', db.updateAccount);
-    app.delete('/account/insurance', db.updateAccount);
+    app.delete('/account/insurance/:route/:id', db.updateAccount);
 
     app.post('/account/contact', db.updateAccount);
     app.put('/account/contact', db.updateAccount);
-    app.delete('/account/contact', db.updateAccount);
+    app.delete('/account/contact/:route/:id', db.updateAccount);
 
     app.post('/account/symptom', db.updateAccount);
     app.put('/account/symptom', db.updateAccount);
-    app.delete('/account/symptom', db.updateAccount);
+    app.delete('/account/symptom/:route/:id', db.updateAccount);
 
     app.post('/account/provider', db.updateAccount);
     app.put('/account/provider', db.updateAccount);
-    app.delete('/account/provider', db.updateAccount);
+    app.delete('/account/provider/:route/:id', db.updateAccount);
 
     app.put('/account/profile/:userId', db.updateProfile);
     app.delete('/account/profile/:userId', db.updateProfile);
