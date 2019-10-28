5e00a72fe35389970bbcdcdd36791844d576293c
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-28 03:16:22 -0700

    set coniditonal to return a message with a Link if user has no info in the database for any of the card components. Message displayed is a link to add info

--- client/src/components/cards/Contacts.js
+++ client/src/components/cards/Contacts.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import {Route,Link} from 'react-router-dom';
+import ContactLink from '../forms/Contact/Link';
 
-export default function ContactsCard(props) { 
-    console.log(props)
+export default function ContactsCard(props) {
+    console.log(props.contact)
+    if(props.contact.length > 0){
+        return (
+            <div className="Contacts card">
+            <h2>Emergency Contacts</h2>
+            {props.contact.map(contact => (
+                <div key={contact._id}>                
+                    <h4>Name: {contact.name}</h4>
+                    <h4>Phone Number: {contact.phone}</h4>
+                    {contact.address && <h4>Address: {contact.address.streetAddress}</h4>}
+                    {contact.address && <h4>&ensp;{contact.address.city}, {contact.address.state} {contact.address.zip}</h4>}
+                    {contact.relationship && <h4>Relationship: {contact.relationship}</h4>}
+                    <button onClick={() => props.deleteObject({_id:contact._id, card:props.card, route:props.route})}>delete</button>
+                    <br/>                
+                </div>
+            ))}
+            </div>
+        )
+    }
+
     return (
         <div className="Contacts card">
-        <h2>Emergency Contacts</h2>
-        {props.contact.map(contact => (
-            <div key={contact._id}>                
-                <h4>Name: {contact.name}</h4>
-                <h4>Phone Number: {contact.phone}</h4>
-                {contact.address && <h4>Address: {contact.address.streetAddress}</h4>}
-                {contact.address && <h4>&ensp;{contact.address.city}, {contact.address.state} {contact.address.zip}</h4>}
-                {contact.relationship && <h4>Relationship: {contact.relationship}</h4>}
-                <button onClick={() => props.deleteObject({_id:contact._id, card:props.card, route:props.route})}>delete</button>
-                <br/>                
-            </div>
-        ))}
+            <h2>Emergency Contacts</h2>
+            <Link to={'/contact'} className="contact-card-link" style={{textDecoration: "none"}}>
+                <div className="contact-card-link-container">
+                    <p className="contact-card-link-add">Click here to add Emergency Contacts!</p>
+                </div>
+            </Link>
         </div>
-    )    
+    )
 };
--- client/src/components/cards/Providers.js
+++ client/src/components/cards/Providers.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 export default function Providers(props) {
+    if(props.providers.length > 0){
+        return (
+            <div className="Providers card">
+                <h2>Medical Providers</h2>
+                {props.providers.map(doc=>{
+                return (<div key={doc._id} className="provider-wrapper">
+                    <h4 className="provider-name">{doc.name}</h4>
+                    {doc.doctorType && <p className="provider-title">{doc.doctorType}</p>}
+                    {doc.phone && <p className="provider-phone">Phone: {doc.phone}</p>}
+                    {doc.email && <p className="provider-email">Email: {doc.email}</p>}
+                    <button onClick={() => props.deleteObject({_id:doc._id, card:props.card, route:props.route})}>delete</button>
+                </div>)
+                })}
+            </div>
+        );
+    }
+
     return (
         <div className="Providers card">
             <h2>Medical Providers</h2>
-            {props.providers.map(doc=>{
-            return (<div key={doc._id} className="provider-wrapper">
-                <h4 className="provider-name">{doc.name}</h4>
-                {doc.doctorType && <p className="provider-title">{doc.doctorType}</p>}
-                {doc.phone && <p className="provider-phone">Phone: {doc.phone}</p>}
-                {doc.email && <p className="provider-email">Email: {doc.email}</p>}
-                <button onClick={() => props.deleteObject({_id:doc._id, card:props.card, route:props.route})}>delete</button>
-            </div>)
-            })}
+            <Link to={'/provider'} className="provider-card-link" style={{textDecoration: "none"}}>
+                <div className="provider-card-link-container">
+                    <p className="provider-card-link-add">Click here to add Medical Providers!</p>
+                </div>
+            </Link>
         </div>
-    );
+    )
 }
