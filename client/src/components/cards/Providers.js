7581427f85c95e5f7cc0b9778ebe51a6dafecde9
Zeb <millerZebadiah@gmail.com>
2019-10-29 15:43:03 -0700

    forgot to save providers

--- client/src/components/cards/Providers.js
+++ client/src/components/cards/Providers.js
@@ -22,23 +22,11 @@ export default function Providers(props) {
     return (
         <div className="Providers card">
             <h2>Medical Providers</h2>
-<<<<<<< HEAD
-            {props.providers.map(doc=>{
-             return (<div key={doc._id} className="provider-wrapper">
-                 <h4 className="provider-name">{doc.name}</h4>
-                {doc.doctorType && <p className="provider-title">{doc.doctorType}</p>}
-                {doc.phone && <p className="provider-phone">Phone: {doc.phone}</p>}
-                {doc.email && <p className="provider-email">Email: {doc.email}</p>}
-                <button onClick={() => props.deleteObject({_id:doc._id, card:props.card, route:props.route})}>delete</button>
-             </div>)
-            })}
-=======
             <Link to={'/provider'} className="provider-card-link" style={{textDecoration: "none"}}>
                 <div className="provider-card-link-container">
                     <p className="provider-card-link-add">Click here to add Medical Providers!</p>
                 </div>
             </Link>
->>>>>>> 96915b7a8a6adee8666878ac77486acf6d710f8e
         </div>
     )
 }
