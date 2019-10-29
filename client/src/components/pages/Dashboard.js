8c97b9ee301fb32bbd7c90d19aa6fe83747d4583
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-29 13:14:57 -0700

    changed deleteObject prop to delete and added in edit prop on all card components

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -9,85 +9,90 @@ import Axios from 'axios';
 
 export default class Dashboard extends Component{
 
-  deleteObject = props=> {   
+    deleteObject = props => {   
     Axios.delete(`/account/${props.card}/${props.route}/${this.props.userId}/${props._id}`).then(user=>{
         this.props.setUser(user.data);
     })    
-  };
-  render() {
+    };
 
-    // console.log("HERE ARE THE USER DETAILS", this.props.user)
-    console.log("DASHBOARD HAS THESE PROPS", this.props);   
-    return (
-      <div className="Dashboard">
-        <Symptoms
-        name={this.props.user.name}
-        symptoms={this.props.user.symptomHistory}
-        card='symptom'
-        deleteObject={this.deleteObject}
-        route='deletesymptom'
-        _id={this.props._id}
-        setUser={this.props.setUser}
-        userId={this.props.userId}
-        isLoggedIn={this.props.isLoggedIn}
-        />
+    editObject = () => {
         
-        <section className="container-right">
-            {/* Future use
+    }
 
-            <MedicalHistory
-            name={this.props.name}
-            user={this.props.user}
-            /> */}
+    render() {
+        // console.log("DASHBOARD HAS THESE PROPS", this.props);   
+        return (
+            <div className="Dashboard">
+            <Symptoms
+            name={this.props.user.name}
+            symptoms={this.props.user.symptomHistory}
+            card='symptom'
+            delete={this.deleteObject}
+            route='deletesymptom'
+            setUser={this.props.setUser}
+            userId={this.props.userId}
+            isLoggedIn={this.props.isLoggedIn}
+            edit={this.editObject}
+            />
+            
+            <section className="container-right">
+                {/* Future use
 
-            <Providers
-            name={this.props.user.name}
-            providers={this.props.user.mediData.doctors}
-            deleteObject={this.deleteObject}
-            card='provider'
-            route='deleteprovider'
-            _id={this.props._id}
-            setUser={this.props.setUser}
-            userId={this.props.userId}
-            isLoggedIn={this.props.isLoggedIn}
+                <MedicalHistory
+                name={this.props.name}
+                user={this.props.user}
+                /> */}
+
+                <Providers
+                name={this.props.user.name}
+                providers={this.props.user.mediData.doctors}
+                delete={this.deleteObject}
+                card='provider'
+                route='deleteprovider'
+                setUser={this.props.setUser}
+                userId={this.props.userId}
+                isLoggedIn={this.props.isLoggedIn}
+                edit={this.editObject}
+                />
+                <Contacts
+                setUser={this.props.setUser}
+                userId={this.props.userId}
+                isLoggedIn={this.props.isLoggedIn}
+                name={this.props.user.name}
+                contact={this.props.user.emergencyContacts}            
+                delete={this.deleteObject}
+                edit={this.editObject}
+                card ='contact'
+                route='deletecontact'
+
+                />
+                <Insurance
+                setUser={this.props.setUser}
+                userId={this.props.userId}
+                isLoggedIn={this.props.isLoggedIn}
+                name={this.props.user.name}
+                insurance={this.props.user.mediData.insurance}
+                delete={this.deleteObject}
+                edit={this.editObject}       
+                card='insurance'
+                route='deleteinsurance'
+
+                />
+            </section>
+            <Nav
+                name={this.props.user.name}
+                menu={this.props.menu}
+                isLoggedIn={this.props.isLoggedIn}
+                formOpen={this.props.formOpen}
+                toggleForm={this.props.toggleForm}
+                userId={this.props.userId}
+                user={this.props.user}
+                setUser={this.props.setUser}
             />
-            <Contacts
-            setUser={this.props.setUser}
-            userId={this.props.userId}
-            isLoggedIn={this.props.isLoggedIn}
-            name={this.props.user.name}
-            contact={this.props.user.emergencyContacts}            
-            deleteObject={this.deleteObject}
-            card ='contact'
-            route='deletecontact'
-            _id={this.props._id}                    
-            />
-            <Insurance
-            setUser={this.props.setUser}
-            userId={this.props.userId}
-            isLoggedIn={this.props.isLoggedIn}
-            name={this.props.user.name}
-            insurance={this.props.user.mediData.insurance}
-            deleteObject={this.deleteObject}           
-            card='insurance'
-            route='deleteinsurance'
-            _id={this.props._id}  
-            />
-        </section>
-        <Nav
-          name={this.props.user.name}
-          menu={this.props.menu}
-          isLoggedIn={this.props.isLoggedIn}
-          formOpen={this.props.formOpen}
-          toggleForm={this.props.toggleForm}
-          userId={this.props.userId}
-          user={this.props.user}
-          setUser={this.props.setUser}
-        />
-        {/* <Forms /> */}
-      </div>
-    );
-  }
+            {/* <Forms /> */}
+            </div>
+        );
+    }
 }
 
 
