9a798e1890a64767ca9c9697cab15619df72a938
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-31 02:33:24 -0700

    changed conditionally rendered items to be h5 elements. removed one FA icon and made it have a conditional icon instead to switch between them. commented out delete button for now as it will be moved later

--- client/src/components/Contact/Card.js
+++ client/src/components/Contact/Card.js
@@ -9,39 +9,33 @@ export default function ContactsCard(props) {
             <div className="contact-card card">
                 <h2>Emergency Contacts</h2>
                 {props.contact.map(contact => (
-                    <div key={contact._id} className="contact-card-item">                
-                        <h4 className="contact-card-item-name">Name: {contact.name}</h4>
-                        <h4 className="contact-card-item-phone">Phone Number: {contact.phone}</h4>
-                        {props.itemIsExpanded[contact._id] && <h4 className="contact-card-item-address">Address: {contact.address.streetAddress}</h4>}
-                        {props.itemIsExpanded[contact._id] && <h4 className="contact-card-item-city">&ensp;{contact.address.city}, {contact.address.state} {contact.address.zip}</h4>}
-                        {props.itemIsExpanded[contact._id] && <h4 className="contact-card-item-relationship">Relationship: {contact.relationship}</h4>}
-                        {props.itemIsExpanded[contact._id] ? 
+                    <div key={contact._id} className="contact-card-item card">                
+                        <h4 className="contact-card-item-name">{contact.name}</h4>
+                        <h4 className="contact-card-item-phone">Phone: {contact.phone}</h4>
+
+                        {props.itemIsExpanded[contact._id] && contact.address.streetAddress &&
+                            <h5 className="contact-card-item-address">Address: {contact.address.streetAddress}</h5>}
+
+                        {props.itemIsExpanded[contact._id] && contact.address.city &&
+                            <h5 className="contact-card-item-city">{' '}{contact.address.city}, {contact.address.state} {contact.address.zip}</h5>}
+
+                        {props.itemIsExpanded[contact._id] && contact.relationship &&
+                            <h5 className="contact-card-item-relationship">Relationship: {contact.relationship}</h5>}
+ 
                             <FontAwesomeIcon
-                            className="contact-card-expand"
-                            icon="angle-double-up"
+                            className="contact-card-expand item-expand item"
+                            icon={props.itemIsExpanded[contact._id] ? "angle-double-up" : "angle-double-down"}
                             size="2x"
                             id={contact._id}
-                            name="contact"
                             onClick={(e)=>props.expand(e)}
                             />
-                            
-                            :
-                            
-                            <FontAwesomeIcon
-                            className="contact-card-expand"
-                            icon="angle-double-down"
-                            size="2x"
-                            id={contact._id}
-                            name="contact"
-                            onClick={(e)=>props.expand(e)}
-                            />
-                        }
-                        <FontAwesomeIcon
+
+                        {/* <FontAwesomeIcon
                         icon="minus-circle"
-                        className = "contact-card-delete"
+                        className = "contact-card-delete item-delete item"
                         size="2x" 
                         onClick={() => props.delete({_id:contact._id, card: props.card, route: props.route})}>
-                        </FontAwesomeIcon>
+                        </FontAwesomeIcon> */}
                         <br/>                
                     </div>
                 ))}
