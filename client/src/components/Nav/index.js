ccd0f19152d6a02c2e17f4f37ba6afcd7ee44d20
Magic-Mayo <mike.mayoii@gmail.com>
2019-10-31 02:33:25 -0700

    added route for each card with params for updating

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ProviderLink from '../Provider/Link';
 import ContactLink from '../Contact/Link';
@@ -60,88 +63,136 @@ class Nav extends Component {
 						toggleForm={this.props.toggleForm}
 					/>
 				</div>
-				<Route
-					exact
-					path={'/provider'}
-					render={() => {
-						return (
-							<ProviderLink
-								isLoggedIn={this.props.isLoggedIn}
-								toggleMenu={this.toggleMenu}
+                <Route
+                    exact
+                    path={'/provider'}
+                    render={() => {
+                        return (
+                            <ProviderLink
+                                isLoggedIn={this.props.isLoggedIn}
+                                toggleMenu={this.toggleMenu}
                                 menuState={this.state.dropDownVisible}
                                 userId={this.props.userId}
                                 setUser={this.props.setUser}
                                 user={this.props.user}
                                 searchOrManual={"search"}
-							/>
-						);
-					}}
-				/>
-				<Route
-					exact
-					path={'/provider/entry'}
-					render={() => {
-						return (
-							<ProviderLink
-								isLoggedIn={this.props.isLoggedIn}
-								toggleMenu={this.toggleMenu}
+                            />
+                        );
+                    }}
+                />
+                <Route
+                    exact
+                    path={'/provider/:id'}
+                    render={() => {
+                        return (
+                            <ProviderLink
+                                isLoggedIn={this.props.isLoggedIn}
+                                toggleMenu={this.toggleMenu}
+                                menuState={this.state.dropDownVisible}
+                                userId={this.props.userId}
+                                setUser={this.props.setUser}
+                                user={this.props.user}
+                                searchOrManual={"entry"}
+                            />
+                        );
+                    }}
+                />
+                <Route
+                    exact
+                    path={'/provider/entry'}
+                    render={() => {
+                        return (
+                            <ProviderLink
+                            isLoggedIn={this.props.isLoggedIn}
+                                toggleMenu={this.toggleMenu}
                                 menuState={this.state.dropDownVisible}
                                 userId={this.props.userId}
                                 setUser={this.props.setUser}
                                 user={this.props.user}
                                 searchOrManual={'entry'}
-							/>
-						);
-					}}
-				/>
-				<Route
-					exact
-					path={'/contact'}
-					render={() => {
-						return (
-							<ContactLink
+                                />
+                        );
+                    }}
+                    />
+                <Route
+                    exact
+                    path={'/contact'}
+                    render={() => {
+                        return (
+                            <ContactLink
                                 setUser={this.props.setUser}
                                 name={this.props.name}
                                 userId={this.props.userId}
                                 isLoggedIn={this.props.isLoggedIn}
                                 toggleMenu={this.toggleMenu}
-								menuState={this.state.dropDownVisible}
-                                user={this.props.user}
-							/>
-						);
-					}}
-				/>
-				<Route 
-					exact 
-					path={'/symptom'} 
-					render={() => {
-						return (
-							<SymptomLink 
-                                isLoggedIn={this.props.isLoggedIn}
-								toggleMenu={this.toggleMenu}
-								userId={this.props.userId}
                                 menuState={this.state.dropDownVisible}
+                                user={this.props.user}
+                            />
+                        );
+                    }}
+                />
+                <Route
+                    exact
+                    path={'/contact/:id'}
+                    render={() => {
+                        return (
+                            <ContactLink
                                 setUser={this.props.setUser}
-							/>
-						)
-					}}
-				/>
-				<Route 
-					exact 
-					path={'/insurance'} 
-					render={() => {
-						return (
-							<InsuranceLink 
+                                name={this.props.name}
+                                userId={this.props.userId}
                                 isLoggedIn={this.props.isLoggedIn}
                                 toggleMenu={this.toggleMenu}
-								menuState={this.state.dropDownVisible}
-								name={this.props.name}
-								userId={this.props.userId}
+                                menuState={this.state.dropDownVisible}
+                                user={this.props.user}
+                            />
+                        );
+                    }}
+                />
+                <Route
+                    exact
+                    path={'/symptom'}
+                    render={() => {
+                        return (
+                            <SymptomLink 
+                                isLoggedIn={this.props.isLoggedIn}
+                                toggleMenu={this.toggleMenu}
+                                userId={this.props.userId}
+                                menuState={this.state.dropDownVisible}
+                                setUser={this.props.setUser}
+                            />
+                        )
+                    }}
+                />
+                <Route exact
+                    path={'/symptom/:id'}
+                    render={() => {
+                        return (
+                            <SymptomLink 
+                                isLoggedIn={this.props.isLoggedIn}
+                                toggleMenu={this.toggleMenu}
+                                userId={this.props.userId}
+                                menuState={this.state.dropDownVisible}
+                                setUser={this.props.setUser}
+                            />
+                        )
+                    }}
+                />
+                <Route 
+                    exact 
+                    path={'/insurance'} 
+                    render={() => {
+                        return (
+                            <InsuranceLink 
+                                isLoggedIn={this.props.isLoggedIn}
+                                toggleMenu={this.toggleMenu}
+                                menuState={this.state.dropDownVisible}
+                                name={this.props.name}
+                                userId={this.props.userId}
                                 user={this.props.user}
                                 setUser={this.props.setUser}
-								/>)
-					}}
-				/>
+                                />)
+                    }}
+                />
             </div>
 		);
 	}
