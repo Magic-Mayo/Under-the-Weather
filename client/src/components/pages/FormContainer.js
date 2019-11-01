fd537590e5a9f5eb33930ad065fe32ad1346daeb
Magic-Mayo <mike.mayoii@gmail.com>
2019-11-01 08:15:14 -0700

    moved toggle for sign up or login from entrance form

--- client/src/components/pages/FormContainer.js
+++ client/src/components/pages/FormContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import EntranceForm from '../Login/EntranceForm';
-import ProviderForm from '../Provider/Form'
 import SymptomForm from '../Symptom/Form'
+import ProviderForm from '../Provider/Form'
 import ContactForm from '../Contact/Form'
 import InsuranceForm from '../Insurance/Form'
 import ProviderManualEntry from '../Provider/ManualEntry';
@@ -9,11 +9,23 @@ import { BrowserRouter as Router, Link} from 'react-router-dom';
 import {withRouter} from 'react-router';
 
 class FormContainer extends Component {
+    state = {
+        loginActive: true,
+		signupActive: false,
+    }
+
 	componentDidMount() {
         if(this.props.menuState){
             this.props.toggleMenu()
         }
     }
+
+    toggleSignUporLogIn = () => {
+        return this.setState({
+            loginActive: !this.state.loginActive, 
+            signupActive: !this.state.signupActive
+        })
+	};
 
 	togglePassword = () => {
 		this.setState({
@@ -26,39 +38,40 @@ class FormContainer extends Component {
     }
 
 	render() {
-        console.log(this.props.match.params.formtype)
+        console.log(this.props.match)
         return (
 			<div className="FormContainer">
-                    {this.props.formOpen && this.props.match.params &&
-                        <Link to="/dashboard" className="form-container-close" title="Close Form">
-                                X
-                        </Link>
-                    }
-					{
-                        this.props.match.params.formtype === 'contact' ? (
-                            <ContactForm {...this.props}/>
-                        ) : this.props.match.params.formtype === 'provider' ? (
-                            <ProviderForm {...this.props}/>
-                        ) : this.props.match.params.formtype === 'symptom' ? (
-                            <SymptomForm {...this.props}/>
-                        ) : this.props.match.params.formtype === 'insurance' ? (
-                            <InsuranceForm {...this.props}/>
-                        ) : this.props.match.params.formtype === 'manual' ? (
-                            <ProviderManualEntry {...this.props}/>
-                        ) 
-                    : 
-                        <EntranceForm
-                            handleLogIn={this.props.handleLogIn}
-                            state={this.state}
-                            message={this.props.message}
-                            setSignUp={this.setSignUp}
-                            setLogIn={this.setLogIn}
-                            setUser={this.props.setUser}
-                            isLoggedIn={this.props.isLoggedIn}
-                            logInNewUser={this.props.logIn}
-                            userId={this.props.userId}
-                        />
-					}
+                <section className={`form-container ${this.state.loginActive ? 'loginActive' : 'signupActive'}`}>
+                {this.props.isLoggedIn &&
+                    <Link to="/dashboard" className="form-container-close" title="Close Form">
+                            X
+                    </Link>}
+                {
+                    this.props.match.params.formtype === 'contact' ? (
+                        <ContactForm {...this.props}/>
+                    ) : this.props.match.params.formtype === 'provider' ? (
+                        <ProviderForm {...this.props}/>
+                    ) : this.props.match.params.formtype === 'symptom' ? (
+                        <SymptomForm {...this.props}/>
+                    ) : this.props.match.params.formtype === 'insurance' ? (
+                        <InsuranceForm {...this.props}/>
+                    ) : this.props.match.params.formtype === 'manual' ? (
+                        <ProviderManualEntry {...this.props}/>
+                    ) 
+                : 
+                    <EntranceForm
+                        handleLogIn={this.props.handleLogIn}
+                        state={this.state}
+                        setSignUp={this.setSignUp}
+                        setLogIn={this.setLogIn}
+                        setUser={this.props.setUser}
+                        logInNewUser={this.props.logIn}
+                        userId={this.props.userId}
+                        loginActive={this.state}
+                        toggleSignUporLogIn={this.toggleSignUporLogIn}
+                    />
+                }
+                </section>
 			</div>
 		);
 	}
