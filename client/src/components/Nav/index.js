5c38a2843541b7f9fb252280d99b43ef5f3b9e14
MagicMayo <mike.mayoii@gmail.com>
2019-11-01 12:29:32 -0700

    refactored and simplified component code

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,65 +1,30 @@
-import React, {Component} from 'react';
-import {
-    BrowserRouter as Router,
-    Link,
-} from 'react-router-dom';
+import React, { Fragment } from 'react';
+import {Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const NavItems = (props) => {
+export default function Nav(props) {
 	return (
-		<div className="expand-items-container" style={{ display: props.show ? 'flex' : 'none' }}>
-			<Link to={'/dashboard/form/provider'} className="expand-items">
-				provider
-			</Link>
+        <div className="Nav">
+            <div className="expand-items-container" style={{ display: props.navOpen ? 'flex' : 'none' }}>
+                <Link to={'/dashboard/form/provider'} className="expand-items">
+                    provider
+                </Link>
 
-			<Link to={'/dashboard/form/contact'} className="expand-items">
-				contact
-			</Link>
+                <Link to={'/dashboard/form/contact'} className="expand-items">
+                    contact
+                </Link>
 
-			<Link to={'/dashboard/form/symptom'} className="expand-items">
-				symptoms
-			</Link>
+                <Link to={'/dashboard/form/symptom'} className="expand-items">
+                    symptoms
+                </Link>
 
-			<Link to={'/dashboard/form/insurance'} className="expand-items">
-				insurance
-			</Link>
-		</div>
-	);
-};
-
-const NavExpand = ({ onClick }) => {
-	return (
-		<div className="expand-container" onClick={onClick}>
-			<FontAwesomeIcon icon="plus" />
-		</div>
-	);
-};
-
-class Nav extends Component {
-	state = {
-		dropDownVisible: false
-    };
-
-    toggleMenu = () =>
-		this.setState((state) => ({
-			dropDownVisible: !state.dropDownVisible
-		}));
-
-	render() {
-		// console.log('NAV HAS THESE PROPS', this.props);		
-		return (
-            <div>
-				<div className="Nav">
-					<NavExpand onClick={this.toggleMenu} />
-					<NavItems
-						toggleMenu={this.toggleMenu}
-						show={this.state.dropDownVisible}
-						toggleForm={this.props.toggleForm}
-					/>
-				</div>
+                <Link to={'/dashboard/form/insurance'} className="expand-items">
+                    insurance
+                </Link>
             </div>
-		);
-	}
-}
-
-export default Nav;
+            <div className="expand-container" onClick={props.toggleNav}>
+                <FontAwesomeIcon icon="plus" />
+            </div>
+        </div>
+	);
+};
