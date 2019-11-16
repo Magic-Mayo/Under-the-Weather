881f08efb258d9b9ba4df56ec43e11bc92f9ab10
Magic-Mayo <mike.mayoii@gmail.com>
2019-11-16 08:40:39 -0700

    changed loading component and positioning

--- client/src/components/icons/loading.js
+++ client/src/components/icons/loading.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import {css} from '@emotion/core';
-import ClipLoader from 'react-spinners/ClipLoader';
+import PacmanLoader from 'react-spinners/PacmanLoader';
 import axios from 'axios';
 
 const override = css`
     display: block;
     margin: 0 auto;
     border-color: red;
+    position: fixed;
+    top: 30%;
+    left: 40%;
+    transform: translateX(-50%);
 `;
 
 class Loading extends Component {
@@ -29,12 +33,12 @@ class Loading extends Component {
     
     render(){
         return (
-            <div className='sweet-loading'>
-              <ClipLoader
+            <div className='loading'>
+              <PacmanLoader
                 css={override}
                 sizeUnit={"px"}
                 size={150}
-                color={'#123abc'}
+                color={'#FF8D7B'}
                 loading={this.props.loading}
               />
             </div> 
