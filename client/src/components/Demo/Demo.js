945dcd94ed905bf30ec4685572ceb8e32bd0d78f
Magic-Mayo <mike.mayoii@gmail.com>
2019-11-24 13:05:13 -0700

    made page to display message and redirect to login

--- client/src/components/Demo/Demo.js
+++ client/src/components/Demo/Demo.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Header from './Header';
+import { Link, Redirect } from 'react-router-dom';
 
 export default function Demo(props){
-	return (
-		<>
-			<Header />
-			<h1>Demo page</h1>
-		</>
-	)
+	if(props.redirect){
+		return (
+			<Redirect to="/login/form"/>
+		)
+	} else {
+		props.redirectToLogin();
+	}
+
+		return (
+			<>
+				<Header />
+				<div className="demo">
+					<h1 className="demo-redirect">Demo page is still under construction.  Please click 
+						<Link to="/login/form"> here </Link>
+					to log in or register if you are not automatically redirected.
+					</h1>
+				</div>
+			</>
+		)
 }
